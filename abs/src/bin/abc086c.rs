019cf6ecc8a25e05239d9dbfaf123ccbbdac7ee6
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-17 20:39:52 +0900

    feat: abs

--- abs/src/bin/abc086c.rs
+++ abs/src/bin/abc086c.rs
@@ -1,5 +1,21 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        coord: [(i32, i32, i32); n],
+    }
+
+    let mut prev = (0, 0, 0);
+    for (t, x, y) in coord {
+        let (pt, px, py) = prev;
+        let dt = t - pt;
+        let dist = (x - px).abs() + (y - py).abs();
+        if dt < dist || dt % 2 != dist % 2 {
+            println!("No");
+            return;
+        }
+        prev = (t, x, y);
+    }
+    println!("Yes")
 }
