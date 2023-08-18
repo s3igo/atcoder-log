ca5f3b1b460f1757a8b3a0c1644380f430bfb517
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-18 23:32:25 +0900

    feat: abc314 ~b

--- contests/abc314/src/bin/b.rs
+++ contests/abc314/src/bin/b.rs
@@ -1,5 +1,23 @@
+use itertools::*;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input!(n: usize);
+    let mut data = vec![(0, 0, vec![]); n];
+    for (i, datum) in data.iter_mut().enumerate() {
+        input! {
+            ci: usize,
+            ai: [usize; ci],
+        }
+        // (index, count, array)
+        *datum = (i + 1, ci, ai);
+    }
+    input!(x: usize);
+
+    data.retain(|(_, _, a)| a.contains(&x));
+    let c_min = data.iter().map(|(_, c, _)| *c).min().unwrap_or(0);
+    data.retain(|(_, c, _)| *c == c_min);
+
+    println!("{}", data.len());
+    println!("{}", data.iter().map(|(i, _, _)| i).join(" "));
 }
