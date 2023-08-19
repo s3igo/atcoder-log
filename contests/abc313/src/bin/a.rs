e19d134f1284bc16880d2f40e149f73621463ead
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-19 13:45:27 +0900

    feat: abc313 ~b

--- contests/abc313/src/bin/a.rs
+++ contests/abc313/src/bin/a.rs
@@ -1,6 +1,15 @@
-use itertools::*;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: i32,
+        first: i32,
+        mut p: [i32; n - 1],
+    }
+
+    p.sort();
+    p.reverse();
+
+    println!("{}", if first > p[0] { 0 } else { p[0] - first + 1 }); // RE
+    println!("{}", (p[0] - first + 1).max(0)); // RE
 }
--- contests/abc313/src/bin/b.rs
+++ contests/abc313/src/bin/b.rs
@@ -2,5 +2,21 @@ use itertools::*;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); m],
+    }
+
+    let losers = ab.iter().map(|(_, b)| b).dedup().collect_vec();
+    let winners = (1..=n).filter(|x| !losers.contains(&x)).collect_vec();
+
+    println!(
+        "{}",
+        if winners.len() > 1 {
+            String::from("-1")
+        } else {
+            winners[0].to_string()
+        }
+    );
 }
