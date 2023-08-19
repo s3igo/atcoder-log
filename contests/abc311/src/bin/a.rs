33ff352822f951b185ef338405f4b19a5b334dab
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-19 16:35:29 +0900

    feat: abc311 ~b

--- contests/abc311/src/bin/a.rs
+++ contests/abc311/src/bin/a.rs
@@ -1,5 +1,23 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {}
+    input! {
+        _: usize,
+        s: Chars,
+    }
+
+    let mut is_complete = (false, false, false);
+    for (i, c) in s.iter().enumerate() {
+        match c {
+            'A' => is_complete.0 = true,
+            'B' => is_complete.1 = true,
+            'C' => is_complete.2 = true,
+            _ => unreachable!(),
+        }
+        if is_complete == (true, true, true) {
+            println!("{}", i + 1);
+            return;
+        }
+    }
+    println!("No");
 }
--- contests/abc311/src/bin/b.rs
+++ contests/abc311/src/bin/b.rs
@@ -1,5 +1,22 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        d: usize,
+        data: [Chars; n],
+    }
+
+    let mut ans = 0;
+    let mut cnt = 0;
+    for i in 0..d {
+        if data.iter().all(|s| s[i] == 'o') {
+            cnt += 1;
+            ans = ans.max(cnt);
+        } else {
+            cnt = 0;
+        }
+    }
+
+    println!("{}", ans);
 }
