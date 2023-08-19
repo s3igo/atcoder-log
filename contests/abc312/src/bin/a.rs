8d24ee785e8fe429ea52f55653a802f6565856fb
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-19 15:51:28 +0900

    feat: abc312 a

--- contests/abc312/src/bin/a.rs
+++ contests/abc312/src/bin/a.rs
@@ -1,5 +1,8 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input!(s: String);
+    let cases = vec!["ACE", "BDF", "CEG", "DFA", "EGB", "FAC", "GBD"];
+
+    println!("{}", if cases.contains(&s.as_str()) { "Yes" } else { "No" });
 }
