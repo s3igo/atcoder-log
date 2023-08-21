2a8e4b00f6081f7e11a9ba778f3b8a9d9e7665b9
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-21 18:11:06 +0900

    feat: abc310 ~b

--- contests/abc310/src/bin/b.rs
+++ contests/abc310/src/bin/b.rs
@@ -1,5 +1,34 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        _: usize,
+    }
+    let mut data = vec![(0, HashSet::<usize>::new()); n];
+    for datum in data.iter_mut() {
+        input! {
+            p: usize,
+            c: usize,
+            fs: [usize; c],
+        }
+        *datum = (p, fs.into_iter().collect::<HashSet<_>>());
+    }
+
+    for (i, j) in data.iter().tuple_combinations() {
+        let check =
+            |(p1, fs1): &(usize, HashSet<usize>), (p2, fs2): &(usize, HashSet<usize>)| -> bool {
+                p1 >= p2
+                    && fs2.is_superset(fs1)
+                    && (p1 > p2 || !fs2.difference(fs1).collect_vec().is_empty())
+            };
+        if check(i, j) || check(j, i) {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
