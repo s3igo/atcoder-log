c40ee3bf171b4f77e97d9eca12f0a7171043d84d
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-22 08:51:57 +0900

    feat: abc308 ~b

--- contests/abc308/src/bin/a.rs
+++ contests/abc308/src/bin/a.rs
@@ -1,5 +1,14 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        s: [usize; 8],
+    }
+
+    let cond = s.iter().tuple_windows().all(|(a, b)| a <= b)
+        && s.iter().all(|x| (100..=675).contains(x))
+        && s.iter().all(|&x| x % 25 == 0);
+
+    println!("{}", if cond { "Yes" } else { "No" });
 }
--- contests/abc308/src/bin/b.rs
+++ contests/abc308/src/bin/b.rs
@@ -1,5 +1,19 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        m: usize,
+        c: [String; n],
+        d: [String; m],
+        p0: usize,
+        p: [usize; m],
+    }
+
+    let dict = d.iter().zip(p.iter()).collect::<HashMap<_, _>>();
+    let ans = c.iter().map(|x| *dict.get(x).unwrap_or(&&p0)).sum::<usize>();
+
+    println!("{}", ans);
 }
