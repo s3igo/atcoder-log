e179969580aa7eb2c6bc1c49d2004caf4496e1c8
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-21 22:22:28 +0900

    feat: abc309 ~b

--- contests/abc309/src/bin/a.rs
+++ contests/abc309/src/bin/a.rs
@@ -1,5 +1,16 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        a: usize,
+        b: usize,
+    }
+
+    let ans = if a % 3 != 0 && b == a + 1 || b % 3 != 1 && a == b - 1 {
+        "Yes"
+    } else {
+        "No"
+    };
+
+    println!("{}", ans);
 }
--- contests/abc309/src/bin/b.rs
+++ contests/abc309/src/bin/b.rs
@@ -1,5 +1,40 @@
-use proconio::input;
+use itertools::{Itertools, Position};
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [Chars; n],
+    }
+
+    let mut ans = a.clone();
+    for (i, e) in a.iter().tuple_windows::<(_, _)>().with_position().enumerate() {
+        match e {
+            Position::Only((prev, next)) => {
+                ans[0][0] = next[0];
+                ans[0][1] = prev[0];
+                ans[1][0] = next[1];
+                ans[1][1] = prev[1];
+                break;
+            },
+            Position::First((prev, next)) => {
+                ans[0].rotate_right(1);
+                ans[0][0] = next[0];
+                ans[1][n - 1] = prev[n - 1];
+            },
+            Position::Middle((prev, next)) => {
+                ans[i][0] = next[0];
+                ans[i + 1][n - 1] = prev[n - 1];
+            },
+            Position::Last((prev, next)) => {
+                ans[n - 1].rotate_left(1);
+                ans[n - 1][n - 1] = prev[n - 1];
+                ans[n - 2][0] = next[0];
+            },
+        }
+    }
+
+    for e in ans {
+        println!("{}", e.iter().join(""));
+    }
 }
