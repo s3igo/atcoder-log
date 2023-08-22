ec022fc0dbeb0d8b8204a9d62bda7db63fcefe2c
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-22 22:50:49 +0900

    feat: abc305 ~b

--- contests/abc305/src/bin/a.rs
+++ contests/abc305/src/bin/a.rs
@@ -1,5 +1,9 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input!(n: f64);
+
+    let ans = (n / 5.).round() as usize * 5;
+
+    println!("{}", ans);
 }
--- contests/abc305/src/bin/b.rs
+++ contests/abc305/src/bin/b.rs
@@ -1,5 +1,16 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        p: char,
+        q: char,
+    }
+    let cases = (b'A'..=b'G').zip([0, 3, 1, 4, 1, 5, 9].iter()).collect_vec();
+
+    let range = (p.min(q) as u8..=p.max(q) as u8).collect_vec();
+    let ans =
+        cases.iter().filter(|(k, _)| range.contains(k)).skip(1).map(|(_, &v)| v).sum::<usize>();
+
+    println!("{}", ans);
 }
