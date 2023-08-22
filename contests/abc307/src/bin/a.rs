3bd6817e59a9ae7b933221d9045da756b6929464
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-22 11:43:02 +0900

    feat: abc307 ~b

--- contests/abc307/src/bin/a.rs
+++ contests/abc307/src/bin/a.rs
@@ -1,5 +1,13 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [[usize; 7]; n],
+    }
+
+    let ans = a.iter().map(|x| x.iter().sum::<usize>()).collect_vec();
+
+    println!("{}", ans.iter().join(" "));
 }
--- contests/abc307/src/bin/b.rs
+++ contests/abc307/src/bin/b.rs
@@ -1,5 +1,18 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        s: [String; n],
+    }
+
+    let cond = s.into_iter().permutations(2).map(|x| x.concat()).any(|x| is_palindrome(&x));
+
+    println!("{}", if cond { "Yes" } else { "No" });
+}
+
+fn is_palindrome(s: &str) -> bool {
+    let s = s.chars().collect::<Vec<char>>();
+    s == s.iter().rev().copied().collect::<Vec<char>>()
 }
