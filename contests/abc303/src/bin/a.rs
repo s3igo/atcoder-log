e4fb5752776026adb069a9e66c2d5e163d7fb27c
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-23 12:00:41 +0900

    feat: abc303 ~b

--- contests/abc303/src/bin/a.rs
+++ contests/abc303/src/bin/a.rs
@@ -1,5 +1,16 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        _: usize,
+        s: String,
+        t: String,
+    }
+
+    let is_similar = |a: char, b: char| -> bool {
+        a == b || a.min(b) == '1' && a.max(b) == 'l' || a.min(b) == '0' && a.max(b) == 'o'
+    };
+    let cond = s.chars().zip(t.chars()).all(|(a, b)| is_similar(a, b));
+
+    println!("{}", if cond { "Yes" } else { "No" });
 }
--- contests/abc303/src/bin/b.rs
+++ contests/abc303/src/bin/b.rs
@@ -1,5 +1,29 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        m: usize,
+        a: [[usize; n]; m],
+    }
+
+    // table[i] = (name, neighbors)
+    let mut table = (1..=n).map(|x| (x, Vec::<usize>::new())).collect_vec();
+    for e in a {
+        for (&prev, &next) in e.iter().tuple_windows() {
+            table[prev - 1].1.push(next);
+            table[next - 1].1.push(prev);
+        }
+    }
+
+    let ans = table.iter().fold(0, |acc, (i, e)| {
+        let mut neighbors = e.iter().cloned().collect::<HashSet<_>>();
+        neighbors.insert(*i);
+        acc + (1..=n).collect::<HashSet<_>>().difference(&neighbors).count()
+    }) / 2;
+
+    println!("{}", ans);
 }
