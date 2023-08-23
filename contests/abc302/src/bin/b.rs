4a859e229672db23da7dff0e6072f6afbc2bd4bb
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-23 15:36:21 +0900

    feat: abc302 a

--- contests/abc302/src/bin/b.rs
+++ contests/abc302/src/bin/b.rs
@@ -1,5 +1,25 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
+// TODO: wip
 fn main() {
-    input! {}
+    input! {
+        h: usize,
+        _w: usize,
+        s: [Chars; h],
+    }
+
+    let s = s
+        .iter()
+        .enumerate()
+        .map(|(i, row)| row.iter().enumerate().map(|(j, c)| ((i, j), c)).collect_vec())
+        .collect_vec();
+    let mut strs = vec![];
+    for row in s {
+        strs.push(row);
+    }
+
+    // for (i, j) in iproduct!(0..h, 0..w) {
+    //     let
+    // }
 }
