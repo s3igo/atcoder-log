5aa08580022f0f90240f3b704eee51ff8f78a954
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-23 17:32:59 +0900

    feat: abc301 ~b

--- contests/abc301/src/bin/a.rs
+++ contests/abc301/src/bin/a.rs
@@ -1,5 +1,28 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        _: usize,
+        s: String,
+    }
+
+    let (t, a) = s.chars().fold((0, 0), |(t, a), c| match c {
+        'T' => (t + 1, a),
+        'A' => (t, a + 1),
+        _ => unreachable!(),
+    });
+    #[allow(clippy::comparison_chain)]
+    let ans = if t == a {
+        match s.chars().rev().next().unwrap() {
+            'T' => 'A',
+            'A' => 'T',
+            _ => unreachable!(),
+        }
+    } else if t > a {
+        'T'
+    } else {
+        'A'
+    };
+
+    println!("{}", ans);
 }
--- contests/abc301/src/bin/b.rs
+++ contests/abc301/src/bin/b.rs
@@ -1,5 +1,26 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut ans = a
+        .iter()
+        .tuple_windows()
+        .flat_map(|(&prev, &next)| {
+            if (prev as isize - next as isize).abs() == 1 {
+                vec![prev]
+            } else if prev < next {
+                (prev..next).collect_vec()
+            } else {
+                ((next + 1)..=prev).rev().collect_vec()
+            }
+        })
+        .collect_vec();
+    ans.push(*a.last().unwrap());
+
+    println!("{}", ans.iter().join(" "));
 }
