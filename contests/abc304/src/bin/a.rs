ac29c17012d06b88f8ee94d0bb12da76b609a4ed
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-23 10:00:07 +0900

    feat: abc304 ~b

--- contests/abc304/src/bin/a.rs
+++ contests/abc304/src/bin/a.rs
@@ -1,5 +1,14 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        sa: [(String, usize); n],
+    }
+
+    let min = sa.iter().map(|(_, a)| a).min().unwrap();
+    let ans = sa.iter().cycle().skip_while(|(_, a)| a != min).take(n).map(|(s, _)| s).collect_vec();
+
+    println!("{}", ans.iter().join("\n"));
 }
--- contests/abc304/src/bin/b.rs
+++ contests/abc304/src/bin/b.rs
@@ -1,5 +1,13 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input!(n: usize);
+    let cases = (3..=9).map(|x| 10_usize.pow(x)).collect_vec();
+
+    let &ceil = cases.iter().find(|&&x| n < x).unwrap();
+    let divisor = ceil / 1000;
+    let ans = (n as f64 / divisor as f64) as usize * divisor;
+
+    println!("{}", ans);
 }
