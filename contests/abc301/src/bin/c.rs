a359396395e22fe7444db5abc3da6873b43b3113
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-24 13:22:21 +0900

    wip: abc301 c

--- contests/abc301/src/bin/c.rs
+++ contests/abc301/src/bin/c.rs
@@ -1,5 +1,30 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        s: String,
+        t: String,
+    }
+
+    let mut s_dict = HashMap::new();
+    for c in s.chars() {
+        s_dict.entry(c).and_modify(|cnt| *cnt += 1).or_insert(1);
+    }
+    let mut t_dict = HashMap::new();
+    for c in t.chars() {
+        t_dict.entry(c).and_modify(|cnt| *cnt += 1).or_insert(1);
+    }
+
+    for c in "atcoder".to_string().chars() {
+        let max = s_dict.get(&c).unwrap_or(&0).max(t_dict.get(&c).unwrap_or(&0));
+        if s_dict.get(&'@').unwrap_or(&0) < max - s_dict.get(&c).unwrap_or(&0)
+            || t_dict.get(&'@').unwrap_or(&0) < max - t_dict.get(&c).unwrap_or(&0)
+        {
+            println!("No");
+            return;
+        }
+    }
 }
