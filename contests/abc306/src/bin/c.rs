b1a04b623f7d3f4ca5d36c6413f7ea2a0617cdcb
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-25 12:19:46 +0900

    refactor: abc306 c

--- contests/abc306/src/bin/c.rs
+++ contests/abc306/src/bin/c.rs
@@ -7,14 +7,32 @@ fn main() {
         a: [usize; 3 * n],
     }
 
-    let mut cnt = vec![0; n];
-    let mut ans = Vec::new();
-    for e in a {
-        cnt[e - 1] += 1;
-        if cnt[e - 1] == 2 {
-            ans.push(e);
-        }
-    }
+    // 別解
+    // let mut cnt = vec![0; n];
+    // let mut ans = Vec::new();
+    // for e in a {
+    //     cnt[e - 1] += 1;
+    //     if cnt[e - 1] == 2 {
+    //         ans.push(e);
+    //     }
+    // }
+
+    let ans = a
+        .iter()
+        .enumerate()
+        .map(|(i, &e)| (i + 1, e))
+        .sorted_by(|(_, a), (_, b)| a.cmp(b))
+        .tuple_windows::<(_, _, _)>()
+        .filter_map(|((_, l), (idx, c), (_, r))| {
+            if l == c && c == r {
+                Some((idx, c))
+            } else {
+                None
+            }
+        })
+        .sorted_by(|(i, _), (j, _)| i.cmp(j))
+        .map(|(_, e)| e)
+        .collect_vec();
 
     println!("{}", ans.iter().join(" "));
 }
