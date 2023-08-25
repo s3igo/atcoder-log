db09d7636cba5bc0439081baa3cd09bfb22b2c13
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-25 11:27:12 +0900

    feat: abc306 c

--- contests/abc306/src/bin/c.rs
+++ contests/abc306/src/bin/c.rs
@@ -1,5 +1,20 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [usize; 3 * n],
+    }
+
+    let mut cnt = vec![0; n];
+    let mut ans = Vec::new();
+    for e in a {
+        cnt[e - 1] += 1;
+        if cnt[e - 1] == 2 {
+            ans.push(e);
+        }
+    }
+
+    println!("{}", ans.iter().join(" "));
 }
