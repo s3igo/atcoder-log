b69d7f424249743629aae1862488d786513da1e4
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-26 16:58:46 +0900

    feat: abc298 a

--- contests/abc298/src/bin/a.rs
+++ contests/abc298/src/bin/a.rs
@@ -1,5 +1,12 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        _: usize,
+        s: String,
+    }
+
+    let cond = s.chars().any(|c| c == 'o') && s.chars().all(|c| c != 'x');
+
+    println!("{}", if cond { "Yes" } else { "No" });
 }
