849498dfe015eab600f1a1d18a7eb158dc44e6e3
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-26 10:02:07 +0900

    feat: abc300 a

--- contests/abc300/src/bin/a.rs
+++ contests/abc300/src/bin/a.rs
@@ -1,5 +1,15 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: usize,
+        b: usize,
+        c: [usize; n],
+    }
+
+    let (ans, _) =
+        c.iter().enumerate().map(|(i, x)| (i + 1, x)).find(|(_, x)| **x == a + b).unwrap();
+
+    println!("{}", ans);
 }
