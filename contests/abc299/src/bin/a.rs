4396a6108cd8d1e9758472cb93daaf58e2c87e08
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-26 16:40:51 +0900

    feat: abc299 ~b

--- contests/abc299/src/bin/a.rs
+++ contests/abc299/src/bin/a.rs
@@ -1,5 +1,13 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        _: usize,
+        s: String
+    }
+
+    let cond =
+        s.chars().skip_while(|&e| e != '|').skip(1).take_while(|&e| e != '|').any(|e| e == '*');
+
+    println!("{}", if cond { "in" } else { "out" });
 }
