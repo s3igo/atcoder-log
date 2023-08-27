b85f2df74177519613c64fee845560cb60848121
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-28 01:28:50 +0900

    feat: abc297 ~b

--- contests/abc297/src/bin/a.rs
+++ contests/abc297/src/bin/a.rs
@@ -1,5 +1,18 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        d: usize,
+        t: [usize; n],
+    }
+
+    let ans = t
+        .iter()
+        .tuple_windows()
+        .find_map(|(l, r)| if r - l <= d { Some(*r as i64) } else { None })
+        .unwrap_or(-1);
+
+    println!("{}", ans);
 }
--- contests/abc297/src/bin/b.rs
+++ contests/abc297/src/bin/b.rs
@@ -1,5 +1,11 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input!(s: String);
+
+    let cond = s.chars().positions(|c| c == 'B').map(|i| i + 1).sum::<usize>() % 2 == 1
+        && s.chars().skip_while(|&c| c != 'R').skip(1).take_while(|&c| c != 'R').any(|c| c == 'K');
+
+    println!("{}", if cond { "Yes" } else { "No" });
 }
