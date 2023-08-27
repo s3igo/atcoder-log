019e3cdf41b94de87d579cb3c759c67729a9c66c
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-27 22:31:28 +0900

    style: remove unnecessary `()`

--- contests/abc299/src/bin/b.rs
+++ contests/abc299/src/bin/b.rs
@@ -9,7 +9,7 @@ fn main() {
         r: [usize; n],
     }
 
-    let data = izip!((1..=n), c.iter(), r.iter()).collect_vec();
+    let data = izip!(1..=n, c.iter(), r.iter()).collect_vec();
     let cmp = if c.contains(&t) { t } else { c[0] };
     let (ans, ..) =
         data.iter().filter(|(_, &c, _)| c == cmp).max_by(|(.., rl), (.., rr)| rl.cmp(rr)).unwrap();
