4cab2d475a1886326a79356eeacb11007dcd6606
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-27 22:30:27 +0900

    feat: abc317 ~b

--- contests/abc317/src/bin/a.rs
+++ contests/abc317/src/bin/a.rs
@@ -1,5 +1,14 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        h: usize,
+        x: usize,
+        p: [usize; n],
+    }
+
+    let ans = p.iter().position(|e| e + h >= x).unwrap() + 1;
+
+    println!("{}", ans);
 }
--- contests/abc317/src/bin/b.rs
+++ contests/abc317/src/bin/b.rs
@@ -1,5 +1,18 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let ans = a
+        .iter()
+        .sorted()
+        .tuple_windows()
+        .find_map(|(&l, &r)| if l + 1 == r { None } else { Some(l + 1) })
+        .unwrap();
+
+    println!("{}", ans);
 }
