d9e0c6a7aa440a9b9e3ba38c93da4488f8c98ca3
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-28 21:02:41 +0900

    feat: abc294 ~b

--- contests/abc294/src/bin/a.rs
+++ contests/abc294/src/bin/a.rs
@@ -1,5 +1,11 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    println!("{}", a.iter().filter(|&e| e % 2 == 0).join(" "));
 }
--- contests/abc294/src/bin/b.rs
+++ contests/abc294/src/bin/b.rs
@@ -1,5 +1,19 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        h: usize,
+        w: usize,
+        a: [[u8; w]; h],
+    }
+
+    let ans = a
+        .iter()
+        .map(|e| {
+            e.iter().map(|&c| if c == 0 { '.' } else { (b'A' - 1 + c) as char }).collect::<String>()
+        })
+        .collect_vec();
+
+    println!("{}", ans.iter().join("\n"));
 }
