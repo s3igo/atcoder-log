7bd27a95de23082197b8bed601b668a2fa2086b6
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-28 20:27:38 +0900

    feat: abc295 ~b

--- contests/abc295/src/bin/a.rs
+++ contests/abc295/src/bin/a.rs
@@ -1,5 +1,13 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        w: [String; n],
+    }
+    let cases = vec!["and", "not", "that", "the", "you"];
+
+    let cond = w.iter().any(|s| cases.contains(&s.as_str()));
+
+    println!("{}", if cond { "Yes" } else { "No" });
 }
--- contests/abc295/src/bin/b.rs
+++ contests/abc295/src/bin/b.rs
@@ -1,5 +1,30 @@
-use proconio::input;
+use itertools::{iproduct, Itertools};
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {}
+    input! {
+        r: usize,
+        c: usize,
+        b: [Chars; r],
+    }
+
+    let mut ans = b.clone();
+    for (i, j) in iproduct!(0..r, 0..c) {
+        if b[i][j].is_ascii_digit() {
+            let might = b[i][j].to_digit(10).unwrap() as usize;
+            for (k, l) in iproduct!(0..r, 0..c) {
+                if manhattan_distance((i, j), (k, l)) <= might {
+                    ans[k][l] = '.'
+                }
+            }
+        }
+    }
+
+    println!("{}", ans.iter().map(|row| row.iter().join("")).join("\n"));
+}
+
+// to support before v1.51.0; `unsigned_abs()` is not available
+#[allow(clippy::cast_abs_to_unsigned)]
+fn manhattan_distance((ax, ay): (usize, usize), (bx, by): (usize, usize)) -> usize {
+    (ax as i64 - bx as i64).abs() as usize + (ay as i64 - by as i64).abs() as usize
 }
