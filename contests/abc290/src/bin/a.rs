122fa4750853db438ec56136ddf6b7819e63518d
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-30 00:43:52 +0900

    feat: abc290 ~b

--- contests/abc290/src/bin/a.rs
+++ contests/abc290/src/bin/a.rs
@@ -1,5 +1,14 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        m: usize,
+        a: [usize; n],
+        b: [usize; m],
+    }
+
+    let ans = a.iter().zip(1..).filter(|(_, i)| b.contains(i)).map(|(e, _)| e).sum::<usize>();
+
+    println!("{}", ans);
 }
--- contests/abc290/src/bin/b.rs
+++ contests/abc290/src/bin/b.rs
@@ -1,5 +1,21 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        _: usize,
+        k: usize,
+        s: String,
+    }
+
+    let cnt = s.chars().filter(|&c| c == 'o').count();
+    let ans = s
+        .chars()
+        .rev()
+        .collect::<String>()
+        .replacen('o', "x", cnt - k)
+        .chars()
+        .rev()
+        .collect::<String>();
+
+    println!("{}", ans);
 }
