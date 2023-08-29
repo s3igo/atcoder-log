49b00c3660c6c760b6d4b33770abc83d6c57065a
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-29 11:50:29 +0900

    feat: abc293 ~b

--- contests/abc293/src/bin/a.rs
+++ contests/abc293/src/bin/a.rs
@@ -1,5 +1,9 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {}
+    input!(s: Chars);
+
+    let ans = s.chunks(2).flat_map(|e| vec![e[1], e[0]]).collect::<String>();
+
+    println!("{}", ans);
 }
--- contests/abc293/src/bin/b.rs
+++ contests/abc293/src/bin/b.rs
@@ -1,5 +1,26 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let ans = a
+        .iter()
+        .enumerate()
+        .fold(vec![false; n], |mut called, (i, e)| {
+            if !called[i] {
+                called[e - 1] = true;
+            }
+            called
+        })
+        .iter()
+        .positions(|&b| !b)
+        .map(|i| i + 1)
+        .collect_vec();
+
+    println!("{}", ans.len());
+    println!("{}", ans.iter().join(" "));
 }
