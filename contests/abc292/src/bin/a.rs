b014c0322eb45cc74b35a602fcffbce0a194fe2e
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-29 12:12:49 +0900

    feat: abc292 ~b

--- contests/abc292/src/bin/a.rs
+++ contests/abc292/src/bin/a.rs
@@ -1,5 +1,7 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input!(s: String);
+
+    println!("{}", s.to_uppercase());
 }
--- contests/abc292/src/bin/b.rs
+++ contests/abc292/src/bin/b.rs
@@ -1,5 +1,19 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        q: usize,
+        event: [(usize, usize); q],
+    }
+
+    let mut players = vec![0; n];
+    for (kind, num) in event {
+        match kind {
+            1 => players[num - 1] += 1,
+            2 => players[num - 1] += 2,
+            3 => println!("{}", if players[num - 1] >= 2 { "Yes" } else { "No" }),
+            _ => unreachable!(),
+        }
+    }
 }
