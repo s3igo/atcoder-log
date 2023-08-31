26c81e87b11226d20c599600d715fedd4b4a049e
s3igo <85787242+s3igo@users.noreply.github.com>
2023-08-31 09:39:00 +0900

    feat: abc287 ~b

--- contests/abc287/src/bin/a.rs
+++ contests/abc287/src/bin/a.rs
@@ -1,5 +1,16 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        s: [String; n],
+    }
+
+    let (f, a) = s.iter().fold((0, 0), |(f, a), e| match e.as_str() {
+        "For" => (f + 1, a),
+        "Against" => (f, a + 1),
+        _ => unreachable!(),
+    });
+
+    println!("{}", if f > a { "Yes" } else { "No" });
 }
--- contests/abc287/src/bin/b.rs
+++ contests/abc287/src/bin/b.rs
@@ -1,5 +1,19 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        m: usize,
+        s: [String; n],
+        t: [String; m],
+    }
+
+    let ans = s
+        .into_iter()
+        .cartesian_product(t.into_iter().unique())
+        .filter(|(s, t)| s.ends_with(t))
+        .count();
+
+    println!("{}", ans);
 }
