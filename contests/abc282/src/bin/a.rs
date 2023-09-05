4cd1e48c5fac48899cc9d28c1d5b5b0f7cd7c058
s3igo <85787242+s3igo@users.noreply.github.com>
2023-09-05 09:45:10 +0900

    feat: abc282 ~b

--- contests/abc282/src/bin/a.rs
+++ contests/abc282/src/bin/a.rs
@@ -1,5 +1,9 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input!(k: u8);
+
+    let ans = (b'A'..b'A' + k).map(|c| c as char).collect::<String>();
+
+    println!("{}", ans);
 }
--- contests/abc282/src/bin/b.rs
+++ contests/abc282/src/bin/b.rs
@@ -1,5 +1,18 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        _: usize,
+        a: [String; n],
+    }
+
+    let ans = a
+        .iter()
+        .tuple_combinations()
+        .filter(|(l, r)| l.chars().zip(r.chars()).all(|(lc, rc)| lc == 'o' || rc == 'o'))
+        .count();
+
+    println!("{}", ans);
 }
