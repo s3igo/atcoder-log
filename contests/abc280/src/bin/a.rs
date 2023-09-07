2b1aa35c05349452b8311a67d86b5c417eb331a7
s3igo <85787242+s3igo@users.noreply.github.com>
2023-09-07 11:26:25 +0900

    feat: abc280 ~b

--- contests/abc280/src/bin/a.rs
+++ contests/abc280/src/bin/a.rs
@@ -1,5 +1,13 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {}
+    input! {
+        h: usize,
+        _: usize,
+        s: [Chars; h],
+    }
+
+    let ans = s.iter().flatten().filter(|&&c| c == '#').count();
+
+    println!("{}", ans);
 }
--- contests/abc280/src/bin/b.rs
+++ contests/abc280/src/bin/b.rs
@@ -1,5 +1,15 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        s: [isize; n],
+    }
+
+    let s = std::iter::once(0).chain(s).collect_vec();
+
+    let ans = s.iter().tuple_windows().map(|(&l, &r)| r - l).collect_vec();
+
+    println!("{}", ans.iter().join(" "));
 }
