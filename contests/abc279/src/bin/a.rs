a33492983652671c744c2d991e60bae527ef627c
s3igo <85787242+s3igo@users.noreply.github.com>
2023-09-08 11:40:47 +0900

    feat: abc279 ~b

--- contests/abc279/src/bin/a.rs
+++ contests/abc279/src/bin/a.rs
@@ -1,5 +1,13 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input!(s: String);
+
+    let ans = s.chars().fold(0, |acc, c| match c {
+        'v' => acc + 1,
+        'w' => acc + 2,
+        _ => unreachable!(),
+    });
+
+    println!("{}", ans);
 }
--- contests/abc279/src/bin/b.rs
+++ contests/abc279/src/bin/b.rs
@@ -1,5 +1,12 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        s: String,
+        t: String,
+    }
+
+    let cond = s.matches(&t).count() >= 1;
+
+    println!("{}", if cond { "Yes" } else { "No" });
 }
