9483b1fc19e7686acf078c12400544ec265abc7b
s3igo <85787242+s3igo@users.noreply.github.com>
2023-09-09 10:20:33 +0900

    feat: abc278 ~b

--- contests/abc278/src/bin/a.rs
+++ contests/abc278/src/bin/a.rs
@@ -1,5 +1,19 @@
+use std::cmp::Ordering;
+
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+
+    let ans = match n.cmp(&k) {
+        Ordering::Greater => a.iter().skip(k).chain(std::iter::repeat(&0).take(k)).collect_vec(),
+        _ => vec![&0; n],
+    };
+
+    println!("{}", ans.iter().join(" "));
 }
--- contests/abc278/src/bin/b.rs
+++ contests/abc278/src/bin/b.rs
@@ -1,5 +1,53 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        mut h: usize,
+        mut m: usize,
+    }
+
+    while is_valid_range(h, m) {
+        if is_mistakable(h, m) {
+            break;
+        }
+
+        let (next_h, next_m) = increment_time((h, m));
+        h = next_h;
+        m = next_m;
+    }
+
+    println!("{} {}", h, m);
+}
+
+fn is_valid_range(h: usize, m: usize) -> bool {
+    (0..24).contains(&h) && (0..60).contains(&m)
+}
+
+fn is_mistakable(h: usize, m: usize) -> bool {
+    let h = match h >= 10 {
+        true => h.to_string(),
+        false => format!("0{}", h),
+    };
+    let m = match m >= 10 {
+        true => m.to_string(),
+        false => format!("0{}", m),
+    };
+
+    let swapped_h = format!("{}{}", h.chars().next().unwrap(), m.chars().next().unwrap());
+    let swapped_m = format!("{}{}", h.chars().last().unwrap(), m.chars().last().unwrap());
+
+    is_valid_range(swapped_h.parse().unwrap(), swapped_m.parse().unwrap())
+}
+
+fn increment_time((mut h, mut m): (usize, usize)) -> (usize, usize) {
+    m += 1;
+    if m == 60 {
+        m = 0;
+        h += 1;
+    }
+    if h == 24 {
+        h = 0;
+    }
+
+    (h, m)
 }
