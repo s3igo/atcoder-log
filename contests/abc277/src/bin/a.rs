5c1676e65f45c09f765b477cceae851e0e03f11a
s3igo <85787242+s3igo@users.noreply.github.com>
2023-09-10 11:48:17 +0900

    feat: abc277 ~b

--- contests/abc277/src/bin/a.rs
+++ contests/abc277/src/bin/a.rs
@@ -1,5 +1,13 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        x: usize,
+        p: [usize; n],
+    }
+
+    let ans = p.iter().position(|&p| p == x).unwrap() + 1;
+
+    println!("{}", ans);
 }
--- contests/abc277/src/bin/b.rs
+++ contests/abc277/src/bin/b.rs
@@ -1,5 +1,20 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        s: [String; n],
+    }
+    let first = "HDCS";
+    let second = "A23456789TJQK";
+
+    let cond = s.iter().all(|s| {
+        let mut chars = s.chars();
+        let f = chars.next().unwrap();
+        let s = chars.next().unwrap();
+        first.contains(f) && second.contains(s)
+    }) && s.iter().sorted().tuple_windows().all(|(a, b)| a != b);
+
+    println!("{}", if cond { "Yes" } else { "No" });
 }
