9f4edc97442e39d723ffafe6abea4f0a11762a9f
s3igo <85787242+s3igo@users.noreply.github.com>
2023-09-10 10:55:10 +0900

    feat: abc319 ~b

--- contests/abc319/src/bin/a.rs
+++ contests/abc319/src/bin/a.rs
@@ -1,5 +1,21 @@
+use maplit::hashmap;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input!(s: String);
+
+    let dict = hashmap! {
+        "tourist" => 3858,
+        "ksun48" => 3679,
+        "Benq" => 3658,
+        "Um_nik" => 3648,
+        "apiad" => 3638,
+        "Stonefeang" => 3630,
+        "ecnerwala" => 3613,
+        "mnbvmar" => 3555,
+        "newbiedmy" => 3516,
+        "semiexp" => 3481,
+    };
+
+    println!("{}", dict.get(&s.as_str()).unwrap());
 }
--- contests/abc319/src/bin/b.rs
+++ contests/abc319/src/bin/b.rs
@@ -1,5 +1,32 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input!(n: usize);
+
+    let divs = divisors(n);
+    let ans = (0..=n)
+        .map(|i| match divs.iter().find(|&x| i % (n / x) == 0).unwrap() {
+            y @ ..=9 => std::char::from_digit(*y as u32, 10).unwrap(),
+            _ => '-',
+        })
+        .collect::<String>();
+
+    println!("{}", ans);
+}
+
+fn divisors(n: usize) -> Vec<usize> {
+    let mut res = vec![];
+    for i in 1..=n {
+        if i * i > n {
+            break;
+        }
+        if n % i == 0 {
+            res.push(i);
+            if i != n / i {
+                res.push(n / i);
+            }
+        }
+    }
+    res.sort();
+    res
 }
