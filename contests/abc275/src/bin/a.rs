d9ac7442e50b32b65c99ef386634075199f155a1
s3igo <85787242+s3igo@users.noreply.github.com>
2023-09-12 21:44:30 +0900

    feat: abc275 a

--- contests/abc275/src/bin/a.rs
+++ contests/abc275/src/bin/a.rs
@@ -1,5 +1,12 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        h: [usize; n],
+    }
+
+    let (_, ans) = h.iter().zip(1..).max_by_key(|(&h, _)| h).unwrap();
+
+    println!("{}", ans);
 }
--- contests/abc275/src/bin/b.rs
+++ contests/abc275/src/bin/b.rs
@@ -1,5 +1,31 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        a: u128,
+        b: u128,
+        c: u128,
+        d: u128,
+        e: u128,
+        f: u128,
+    }
+    let div = 998244353;
+
+    let abc = a % div * (b % div) % div;
+    let abc = c % div * abc % div;
+
+    let def = d % div * (e % div) % div;
+    let def = f % div * def % div;
+
+    let ans = abc.abs_diff(def) % div;
+
+    // NOTE: WA
+    // input! {
+    //     abc: [u128; 3],
+    //     def: [u128; 3],
+    // }
+    // let abc = abc.iter().fold(1, |acc, &x| x % div * acc) % div;
+    // let def = def.iter().fold(1, |acc, &x| x % div * acc) % div;
+
+    println!("{}", ans);
 }
