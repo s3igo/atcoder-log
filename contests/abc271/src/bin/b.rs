217531e66dfe8694585945d8af88816ae400bdce
s3igo <85787242+s3igo@users.noreply.github.com>
2023-09-16 10:34:45 +0900

    feat: abc271 ~b

--- contests/abc271/src/bin/b.rs
+++ contests/abc271/src/bin/b.rs
@@ -1,5 +1,25 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        q: usize,
+    }
+
+    let mut a = vec![];
+    for _ in 0..n {
+        input! {
+            l: usize,
+            a_in: [usize; l],
+        }
+        a.push(a_in);
+    }
+
+    for _ in 0..q {
+        input! {
+            s: Usize1,
+            t: Usize1,
+        }
+        println!("{}", a[s][t]);
+    }
 }
