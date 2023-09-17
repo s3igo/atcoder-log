7b101de3741de752b96a456c9f1ba355eb2d6002
s3igo <85787242+s3igo@users.noreply.github.com>
2023-09-17 10:47:04 +0900

    feat: abc270 ~b

--- contests/abc270/src/bin/b.rs
+++ contests/abc270/src/bin/b.rs
@@ -1,5 +1,18 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        goal: isize,
+        wall: isize,
+        hammer: isize,
+    }
+
+    match (goal, wall, hammer) {
+        (g, w, _) if g.abs() <= w.abs() || w.signum() != g.signum() => println!("{}", g.abs()),
+        (_, w, h) if h.abs() <= w.abs() => match (h.signum(), w.signum()) {
+            (hs, ws) if hs == ws => println!("{}", goal.abs()),
+            _ => println!("{}", hammer.abs() * 2 + goal.abs()),
+        },
+        _ => println!("{}", -1),
+    }
 }
