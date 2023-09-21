5762e6f30244e6b9fe6dc6c43f2ac5a3f081d1eb
s3igo <85787242+s3igo@users.noreply.github.com>
2023-09-22 00:11:58 +0900

    feat: abc265 ~b

--- contests/abc265/src/bin/a.rs
+++ contests/abc265/src/bin/a.rs
@@ -1,5 +1,17 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        x: usize,
+        y: usize,
+        n: usize,
+    }
+
+    let ans = if x * 3 <= y {
+        x * n
+    } else {
+        n / 3 * y + n % 3 * x
+    };
+
+    println!("{}", ans);
 }
--- contests/abc265/src/bin/b.rs
+++ contests/abc265/src/bin/b.rs
@@ -1,5 +1,22 @@
+use std::collections::HashMap;
+
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        m: usize,
+        t: isize,
+        a: [isize; n - 1],
+        xy: [(usize, isize); m],
+    }
+
+    let xy = xy.into_iter().collect::<HashMap<_, _>>();
+
+    let cond = a.iter().zip(2..).try_fold(t, |acc, (a, i)| match acc - a {
+        ..=0 => None,
+        acc => Some(acc + *xy.get(&i).unwrap_or(&0)),
+    });
+
+    println!("{}", if cond.is_some() { "Yes" } else { "No" });
 }
