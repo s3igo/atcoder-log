5810e38beea8c304a4a13c1a7997a36c73b4976e
s3igo <85787242+s3igo@users.noreply.github.com>
2023-09-27 00:38:04 +0900

    feat: abc263 ~b

--- contests/abc263/src/bin/a.rs
+++ contests/abc263/src/bin/a.rs
@@ -1,5 +1,14 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input!(cards: [usize; 5]);
+
+    let cnt = cards.into_iter().counts();
+    let set = cnt.into_values().collect::<HashSet<_>>();
+    let cond = set == HashSet::from([2, 3]);
+
+    println!("{}", if cond { "Yes" } else { "No" });
 }
--- contests/abc263/src/bin/b.rs
+++ contests/abc263/src/bin/b.rs
@@ -1,5 +1,17 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        p: [usize; n - 1],
+    }
+
+    println!("{}", trace(&p, n, 0));
+}
+
+fn trace(p: &[usize], idx: usize, cnt: usize) -> usize {
+    match idx {
+        1 => cnt,
+        _ => trace(p, p[idx - 2], cnt + 1),
+    }
 }
