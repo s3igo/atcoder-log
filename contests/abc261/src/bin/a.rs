744c18eacf5829df07c09874068b6e2b420417a7
s3igo <85787242+s3igo@users.noreply.github.com>
2023-09-30 15:11:28 +0900

    feat: abc261 ~b

--- contests/abc261/src/bin/a.rs
+++ contests/abc261/src/bin/a.rs
@@ -1,5 +1,19 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        l1: usize,
+        r1: usize,
+        l2: usize,
+        r2: usize,
+    }
+
+    let red = (l1..=r1).collect::<HashSet<_>>();
+    let blue = (l2..=r2).collect::<HashSet<_>>();
+
+    let ans = (&red & &blue).len().saturating_sub(1);
+
+    println!("{}", ans);
 }
--- contests/abc261/src/bin/b.rs
+++ contests/abc261/src/bin/b.rs
@@ -1,5 +1,28 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [Chars; n],
+    }
+
+    let cond = transpose(a.clone())
+        .iter()
+        .flat_map(|row| {
+            row.iter().map(|c| match c {
+                'W' => 'L',
+                'L' => 'W',
+                &c => c,
+            })
+        })
+        .zip(a.into_iter().flatten())
+        .all(|(c1, c2)| c1 == c2);
+
+    println!("{}", if cond { "correct" } else { "incorrect" });
+}
+
+fn transpose<T: Copy>(matrix: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let n = matrix.len();
+    assert!(matrix.iter().all(|row| row.len() == n));
+    (0..n).map(|i| (0..n).map(|j| matrix[j][i]).collect()).collect()
 }
