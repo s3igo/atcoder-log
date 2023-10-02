0f54deacb168ef876e2c89951d57b8c4f4971fd7
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-03 02:45:14 +0900

    feat: abc258 a

--- contests/1.70.0/abc258/src/bin/a.rs
+++ contests/1.70.0/abc258/src/bin/a.rs
@@ -1,5 +1,21 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input!(k: usize);
+
+    let (h, m) = (0..k).fold((21, 0), |(h, m), _| increment(h, m));
+
+    println!("{:02}:{:02}", h, m);
+}
+
+fn increment(mut h: usize, mut m: usize) -> (usize, usize) {
+    m += 1;
+    if m == 60 {
+        m = 0;
+        h += 1;
+    }
+    if h == 24 {
+        h = 0;
+    }
+    (h, m)
 }
