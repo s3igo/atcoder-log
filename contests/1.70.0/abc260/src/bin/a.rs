48f1ec0ce58373405517b25c8f8d024cb24669ff
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-02 22:26:44 +0900

    feat: abc260 ~b

--- contests/1.70.0/abc260/src/bin/a.rs
+++ contests/1.70.0/abc260/src/bin/a.rs
@@ -1,5 +1,13 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input! {}
+    input!(s: String);
+
+    let ans = match s.chars().counts().iter().find(|(_, &v)| v == 1) {
+        Some((k, _)) => k.to_string(),
+        None => "-1".to_string(),
+    };
+
+    println!("{}", ans);
 }
--- contests/1.70.0/abc260/src/bin/b.rs
+++ contests/1.70.0/abc260/src/bin/b.rs
@@ -1,5 +1,25 @@
+use std::cmp::Reverse;
+
+use itertools::{izip, Itertools};
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        x: usize,
+        y: usize,
+        z: usize,
+        a: [usize; n],
+        b: [usize; n],
+    }
+
+    let mut students = izip!(1.., a, b).collect_vec();
+
+    students.sort_by_key(|&(i, a, _)| (Reverse(a), i));
+    students[x..].sort_by_key(|&(i, _, b)| (Reverse(b), i));
+    students[x + y..].sort_by_key(|&(i, a, b)| (Reverse(a + b), i));
+
+    let ans = students.iter().take(x + y + z).map(|(i, ..)| i).sorted().collect_vec();
+
+    println!("{}", ans.iter().join("\n"));
 }
