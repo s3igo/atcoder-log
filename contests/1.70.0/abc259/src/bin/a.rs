e8e4444306fb236112a32b367b564a0b8b86844c
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-03 02:19:37 +0900

    chore: migrate 1.42 -> 1.70

--- contests/1.70.0/abc259/src/bin/a.rs
+++ contests/1.70.0/abc259/src/bin/a.rs
@@ -1,5 +1,20 @@
 use proconio::input;
 
 fn main() {
-    input! {}
+    input! {
+        _: usize,
+        m: usize,
+        x: usize,
+        t: usize,
+        d: usize,
+    }
+
+    if m >= x {
+        println!("{}", t);
+        return;
+    }
+
+    let ans = t - d * (x - m);
+
+    println!("{}", ans);
 }
