cfcb01e0f6e21560379d9002add6c21dfd814de5
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-06 17:45:00 +0900

    chore: migrate contest 1.42 -> 1.70

---
+++ contests/1.70.0/abc307/src/bin/b.rs
@@ -0,0 +1,14 @@
+use itertools::Itertools;
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        s: [String; n],
+    }
+
+    let cond =
+        s.into_iter().permutations(2).map(|x| x.concat()).any(|x| x.chars().eq(x.chars().rev()));
+
+    println!("{}", if cond { "Yes" } else { "No" });
+}
