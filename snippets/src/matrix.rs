17da752ceaf1ecd7ac1572ac9a0fa58342b85528
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-07 13:16:55 +0900

    refactor: apply clippy suggestions

--- snippets/src/matrix.rs
+++ snippets/src/matrix.rs
@@ -21,5 +21,5 @@ fn test_rotate() {
     assert_eq!(rotate(matrix.clone(), 1), vec![vec![7, 4, 1], vec![8, 5, 2], vec![9, 6, 3],]);
     assert_eq!(rotate(matrix.clone(), 2), vec![vec![9, 8, 7], vec![6, 5, 4], vec![3, 2, 1],]);
     assert_eq!(rotate(matrix.clone(), 3), vec![vec![3, 6, 9], vec![2, 5, 8], vec![1, 4, 7],]);
-    assert_eq!(rotate(matrix.clone(), 4), vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9],]);
+    assert_eq!(rotate(matrix, 4), vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9],]);
 }
