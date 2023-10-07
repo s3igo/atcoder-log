c201a1ceb9cdccf8d677b6b84201004f59e3b106
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-08 01:25:40 +0900

    refactor: abc323

--- contests/1.70.0/abc323/src/bin/b.rs
+++ contests/1.70.0/abc323/src/bin/b.rs
@@ -8,8 +8,7 @@ fn main() {
         .iter()
         .map(|s| s.chars().filter(|&c| c == 'o').count())
         .zip(1..)
-        .sorted_by_key(|&(x, i)| (x, -i))
-        .rev()
+        .sorted_by_key(|&(x, i)| (-(x as isize), i))
         .map(|(_, i)| i)
         .collect_vec();
 
--- contests/1.70.0/abc323/src/bin/c.rs
+++ contests/1.70.0/abc323/src/bin/c.rs
@@ -3,29 +3,22 @@ use itertools_num::ItertoolsNum;
 use proconio::input;
 
 fn main() {
-    input!(n: usize, m: usize, a: [isize; m], s: [String; n]);
+    input!(n: usize, m: usize, a: [usize; m], s: [String; n]);
 
     let points = s
         .iter()
-        .map(|s| s.chars().positions(|c| c == 'o').fold(0, |acc, x| acc + a[x]))
+        .map(|s| s.chars().positions(|c| c == 'o').map(|x| a[x]).sum::<usize>())
         .zip(1..)
         .map(|(x, i)| x + i)
         .collect_vec();
 
-    for i in 0..n {
-        let cumsum = s[i]
-            .chars()
-            .positions(|c| c == 'x')
-            .map(|x| a[x])
-            .sorted()
-            .rev()
-            .cumsum::<isize>()
-            .collect_vec();
+    for (i, s) in s.iter().enumerate() {
+        let cumsum =
+            s.chars().positions(|c| c == 'x').map(|x| a[x]).sorted().rev().cumsum::<usize>();
         let mut points = points.clone();
         let cur = points.remove(i);
         let &max = points.iter().max().unwrap();
-        let ans =
-            if cur > max { 0 } else { cumsum.iter().take_while(|&&x| x <= max - cur).count() + 1 };
+        let ans = if cur > max { 0 } else { cumsum.take_while(|&x| x <= max - cur).count() + 1 };
 
         println!("{}", ans);
     }
