dd4b7f79c301525da9ce12c56ca2a860b4be084c
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-07 12:33:01 +0900

    refactor: abc321

--- contests/1.70.0/abc321/src/bin/b.rs
+++ contests/1.70.0/abc321/src/bin/b.rs
@@ -1,17 +1,20 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!(n: usize, x: isize, a: [isize; n - 1]);
+    input!(n: usize, x: usize, a: [usize; n - 1]);
 
-    let ans = (0..=100)
+    let result = (0..=100)
         .filter(|&i| {
-            let mut a = a.clone();
-            a.push(i);
-            a.sort();
-            a[1..n - 1].iter().sum::<isize>() >= x
+            let points = a.clone().into_iter().chain(std::iter::once(i)).collect_vec();
+            points.iter().sorted().dropping(1).dropping_back(1).sum::<usize>() >= x
         })
-        .min()
-        .unwrap_or(-1);
+        .min();
+
+    let ans = match result {
+        Some(x) => x.to_string(),
+        None => "-1".to_string(),
+    };
 
     println!("{}", ans);
 }
