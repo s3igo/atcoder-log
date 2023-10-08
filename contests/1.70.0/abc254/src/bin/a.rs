166131ae2e8b9ccd5842ff066c6a3e53e1b07cf5
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-08 10:08:32 +0900

    feat: abc254 ~b

--- contests/1.70.0/abc254/src/bin/a.rs
+++ contests/1.70.0/abc254/src/bin/a.rs
@@ -1,5 +1,7 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: String);
+
+    println!("{}", &n[1..]);
 }
--- contests/1.70.0/abc254/src/bin/b.rs
+++ contests/1.70.0/abc254/src/bin/b.rs
@@ -1,5 +1,20 @@
+use itertools::Itertools;
+use memoise::memoise;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize);
+
+    for i in 0..n {
+        let ans = (0..=i).map(|j| solve(i, j)).join(" ");
+        println!("{}", ans);
+    }
+}
+
+#[memoise(i < 30, j < 30)]
+fn solve(i: usize, j: usize) -> usize {
+    match (i, j) {
+        (i, j) if j == 0 || i == j => 1,
+        _ => solve(i - 1, j - 1) + solve(i - 1, j),
+    }
 }
