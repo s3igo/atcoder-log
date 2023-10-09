30dfe3ca77f2f55e40b99b85483c1585c3a4ed51
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-10 02:06:05 +0900

    feat: abc253 ~b

--- contests/1.70.0/abc253/src/bin/a.rs
+++ contests/1.70.0/abc253/src/bin/a.rs
@@ -1,5 +1,8 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(a: usize, b: usize, c: usize);
+    let cond = (a.min(c)..=a.max(c)).contains(&b);
+
+    println!("{}", if cond { "Yes" } else { "No" });
 }
--- contests/1.70.0/abc253/src/bin/b.rs
+++ contests/1.70.0/abc253/src/bin/b.rs
@@ -1,5 +1,21 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input!();
+    input!(h: usize, w: usize, s: [Chars; h]);
+
+    let Some((a, b)) = s
+        .iter()
+        .flatten()
+        .positions(|&c| c == 'o')
+        .map(|x| (x / w, x % w))
+        .collect_tuple() else { unreachable!() };
+
+    let ans = manhattan_dst(a, b);
+
+    println!("{}", ans);
+}
+
+fn manhattan_dst((ax, ay): (usize, usize), (bx, by): (usize, usize)) -> usize {
+    ax.abs_diff(bx) + ay.abs_diff(by)
 }
