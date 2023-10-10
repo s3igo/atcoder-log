4b5ebfca2da18250c22802a283eaa8859c49a949
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-10 12:32:33 +0900

    refactor: inline variable

--- contests/1.70.0/abc263/src/bin/a.rs
+++ contests/1.70.0/abc263/src/bin/a.rs
@@ -6,9 +6,8 @@ use proconio::input;
 fn main() {
     input!(cards: [usize; 5]);
 
-    let cnt = cards.into_iter().counts();
-    let set = cnt.into_values().collect::<HashSet<_>>();
-    let cond = set == HashSet::from([2, 3]);
+    let dict = cards.iter().counts();
+    let cond = dict.into_values().collect::<HashSet<_>>() == HashSet::from([2, 3]);
 
     println!("{}", if cond { "Yes" } else { "No" });
 }
