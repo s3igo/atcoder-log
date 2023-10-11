27d4c9b3e3aa3d1f9108c10e1db360df3c4b36d4
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-12 02:19:31 +0900

    feat: abc251 ~b

--- contests/1.70.0/abc251/src/bin/a.rs
+++ contests/1.70.0/abc251/src/bin/a.rs
@@ -1,5 +1,9 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(s: String);
+
+    let ans = s.chars().cycle().take(6).collect::<String>();
+
+    println!("{}", ans);
 }
--- contests/1.70.0/abc251/src/bin/b.rs
+++ contests/1.70.0/abc251/src/bin/b.rs
@@ -1,5 +1,15 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, w: usize, a: [usize; n]);
+
+    let ans = (1..=3)
+        .flat_map(|i| a.iter().combinations(i))
+        .map(|x| x.into_iter().sum::<usize>())
+        .filter(|&x| x <= w)
+        .unique()
+        .count();
+
+    println!("{}", ans);
 }
