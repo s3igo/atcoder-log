b11692319352914b6ca2728916274182b75323bc
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-13 11:35:16 +0900

    feat: abc250 ~b

--- contests/1.70.0/abc250/src/bin/a.rs
+++ contests/1.70.0/abc250/src/bin/a.rs
@@ -1,5 +1,9 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(h: usize, w: usize, r: usize, c: usize);
+
+    let ans = [(r, 1), (r, h), (c, 1), (c, w)].iter().filter(|(a, b)| a != b).count();
+
+    println!("{}", ans);
 }
--- contests/1.70.0/abc250/src/bin/b.rs
+++ contests/1.70.0/abc250/src/bin/b.rs
@@ -1,5 +1,16 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, a: usize, b: usize);
+
+    let pair = [vec!['.'; b], vec!['#'; b]];
+    let row = pair.iter().cycle();
+
+    for i in 0..n {
+        let row = row.clone().skip(i % 2).take(n).flatten().join("");
+        for _ in 0..a {
+            println!("{}", row);
+        }
+    }
 }
