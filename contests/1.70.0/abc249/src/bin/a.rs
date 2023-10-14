d0f0803477f447dc5f0a5fa0480f86b52983dd98
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-14 11:53:47 +0900

    feat: abc249 ~b

--- contests/1.70.0/abc249/src/bin/a.rs
+++ contests/1.70.0/abc249/src/bin/a.rs
@@ -1,5 +1,19 @@
-use proconio::input;
+use std::cmp::Ordering;
+
+use proconio::{input, marker::Usize1};
 
 fn main() {
-    input!();
+    input!(a: usize, b: usize, c: usize, d: usize, e: usize, f: usize, x: Usize1);
+
+    let ans = match distance(a, b, c, x).cmp(&distance(d, e, f, x)) {
+        Ordering::Greater => "Takahashi",
+        Ordering::Equal => "Draw",
+        Ordering::Less => "Aoki",
+    };
+
+    println!("{}", ans);
+}
+
+fn distance(span: usize, speed: usize, rest: usize, time: usize) -> usize {
+    (0..=time).fold(0, |acc, i| if i % (span + rest) < span { acc + speed } else { acc })
 }
--- contests/1.70.0/abc249/src/bin/b.rs
+++ contests/1.70.0/abc249/src/bin/b.rs
@@ -1,5 +1,12 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(s: String);
+
+    let cond = s.chars().any(|c| c.is_ascii_uppercase())
+        && s.chars().any(|c| c.is_ascii_lowercase())
+        && s.chars().all_unique();
+
+    println!("{}", if cond { "Yes" } else { "No" });
 }
