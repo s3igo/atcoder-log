05ff458e8c5332e28638fb7a066715be7f46eeca
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-14 13:35:38 +0900

    fix: input

--- contests/1.70.0/abc249/src/bin/a.rs
+++ contests/1.70.0/abc249/src/bin/a.rs
@@ -1,9 +1,9 @@
 use std::cmp::Ordering;
 
-use proconio::{input, marker::Usize1};
+use proconio::input;
 
 fn main() {
-    input!(a: usize, b: usize, c: usize, d: usize, e: usize, f: usize, x: Usize1);
+    input!(a: usize, b: usize, c: usize, d: usize, e: usize, f: usize, x: usize);
 
     let ans = match distance(a, b, c, x).cmp(&distance(d, e, f, x)) {
         Ordering::Greater => "Takahashi",
@@ -15,5 +15,5 @@ fn main() {
 }
 
 fn distance(span: usize, speed: usize, rest: usize, time: usize) -> usize {
-    (0..=time).fold(0, |acc, i| if i % (span + rest) < span { acc + speed } else { acc })
+    (0..time).fold(0, |acc, i| if i % (span + rest) < span { acc + speed } else { acc })
 }
