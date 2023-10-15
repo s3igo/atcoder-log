501630c755d1b2aa2c88623c6fe031fa6a4088e2
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-16 01:33:57 +0900

    feat: abc248 ~b

--- contests/1.70.0/abc248/src/bin/a.rs
+++ contests/1.70.0/abc248/src/bin/a.rs
@@ -1,5 +1,10 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(s: String);
+
+    let ans = ('0'..='9').find(|x| !s.chars().contains(x)).unwrap();
+
+    println!("{}", ans);
 }
--- contests/1.70.0/abc248/src/bin/b.rs
+++ contests/1.70.0/abc248/src/bin/b.rs
@@ -1,5 +1,17 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(a: usize, b: usize, k: usize);
+
+    let solve = |n| solve(n, b, k, 0);
+
+    println!("{}", solve(a));
+}
+
+fn solve(a: usize, b: usize, k: usize, cnt: usize) -> usize {
+    if a < b {
+        solve(a * k, b, k, cnt + 1)
+    } else {
+        cnt
+    }
 }
