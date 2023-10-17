7124bf68abc6174253c58108db95899858851b94
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-18 01:17:47 +0900

    feat: abc246 ~b

--- contests/1.70.0/abc246/src/bin/a.rs
+++ contests/1.70.0/abc246/src/bin/a.rs
@@ -1,5 +1,9 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(xy: [(isize, isize); 3]);
+
+    let (x, y) = xy.into_iter().reduce(|(acc_x, acc_y), (x, y)| (acc_x ^ x, acc_y ^ y)).unwrap();
+
+    println!("{} {}", x, y);
 }
--- contests/1.70.0/abc246/src/bin/b.rs
+++ contests/1.70.0/abc246/src/bin/b.rs
@@ -1,5 +1,10 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(a: usize, b: usize);
+
+    let d = ((a * a + b * b) as f64).sqrt();
+    let (x, y) = (a as f64 / d, b as f64 / d);
+
+    println!("{} {}", x, y);
 }
