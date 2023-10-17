c4c6d1c1f51a1577a75932cc18476ac4940671f0
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-17 20:19:47 +0900

    feat: abc247 ~b

--- contests/1.70.0/abc247/src/bin/a.rs
+++ contests/1.70.0/abc247/src/bin/a.rs
@@ -1,5 +1,9 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(s: String);
+
+    let ans = std::iter::once('0').chain(s.chars()).take(4).collect::<String>();
+
+    println!("{}", ans);
 }
--- contests/1.70.0/abc247/src/bin/b.rs
+++ contests/1.70.0/abc247/src/bin/b.rs
@@ -1,5 +1,16 @@
+use hashbag::HashBag;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, st: [(String, String); n]);
+
+    let bag = st.iter().flat_map(|(s, t)| [s, t]).collect::<HashBag<_>>();
+    let cond = st.iter().all(|(s, t)| {
+        let mut bag = bag.clone();
+        bag.remove(s);
+        bag.remove(t);
+        bag.get(s).is_none() || bag.get(t).is_none()
+    });
+
+    println!("{}", if cond { "Yes" } else { "No" });
 }
