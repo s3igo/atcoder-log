fa24fb7aa8f6f84f056259e379d83d851713b032
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-20 16:04:35 +0900

    refactor: abc244

--- contests/1.70.0/abc244/src/bin/b.rs
+++ contests/1.70.0/abc244/src/bin/b.rs
@@ -3,15 +3,12 @@ use proconio::input;
 fn main() {
     input!(_: usize, t: String);
 
-    let (x, y, _) = t.chars().fold((0, 0, 0), |(x, y, r_cnt), c| match c {
-        'S' => match r_cnt % 4 {
-            0 => (x + 1, y, r_cnt),
-            1 => (x, y - 1, r_cnt),
-            2 => (x - 1, y, r_cnt),
-            3 => (x, y + 1, r_cnt),
-            _ => unreachable!(),
-        },
-        'R' => (x, y, r_cnt + 1),
+    let (x, y, _) = t.chars().fold((0, 0, 0), |(x, y, r_cnt), c| match (c, r_cnt % 4) {
+        ('R', _) => (x, y, r_cnt + 1),
+        ('S', 0) => (x + 1, y, r_cnt),
+        ('S', 1) => (x, y - 1, r_cnt),
+        ('S', 2) => (x - 1, y, r_cnt),
+        ('S', 3) => (x, y + 1, r_cnt),
         _ => unreachable!(),
     });
 
