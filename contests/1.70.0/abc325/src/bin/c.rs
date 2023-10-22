91caa512bfafc876768c3ed60a31c78f6ba5d70b
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-22 09:46:58 +0900

    refactor: simplify code

--- contests/1.70.0/abc325/src/bin/c.rs
+++ contests/1.70.0/abc325/src/bin/c.rs
@@ -21,18 +21,11 @@ fn trace(i: usize, j: usize, h: usize, w: usize, s: &Vec<Vec<char>>, checked: &m
     if s[i][j] != checked[i][j] {
         checked[i][j] = '#';
         trace(i.saturating_sub(1), j, h, w, s, checked);
-        trace(i.saturating_sub(1), if j < w - 1 { j + 1 } else { j }, h, w, s, checked);
-        trace(i, if j < w - 1 { j + 1 } else { j }, h, w, s, checked);
-        trace(
-            if i < h - 1 { i + 1 } else { i },
-            if j < w - 1 { j + 1 } else { j },
-            h,
-            w,
-            s,
-            checked,
-        );
-        trace(if i < h - 1 { i + 1 } else { i }, j, h, w, s, checked);
-        trace(if i < h - 1 { i + 1 } else { i }, j.saturating_sub(1), h, w, s, checked);
+        trace(i.saturating_sub(1), (j + 1).min(w - 1), h, w, s, checked);
+        trace(i, (j + 1).min(w - 1), h, w, s, checked);
+        trace((i + 1).min(h - 1), (j + 1).min(w - 1), h, w, s, checked);
+        trace((i + 1).min(h - 1), j, h, w, s, checked);
+        trace((i + 1).min(h - 1), j.saturating_sub(1), h, w, s, checked);
         trace(i, j.saturating_sub(1), h, w, s, checked);
         trace(i.saturating_sub(1), j.saturating_sub(1), h, w, s, checked);
     }
