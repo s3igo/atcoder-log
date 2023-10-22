46efbedc8c287b15ce73737e512fbc36658453a0
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-22 11:44:21 +0900

    feat: abc243 ~b

--- contests/1.70.0/abc243/src/bin/a.rs
+++ contests/1.70.0/abc243/src/bin/a.rs
@@ -1,5 +1,24 @@
+use std::ops::ControlFlow;
+
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(v: isize, abc: [isize; 3]);
+
+    let result = abc.iter().cycle().try_fold((v, 0), |(rest, cnt), x| {
+        if rest < 0 {
+            ControlFlow::Break(cnt)
+        } else {
+            ControlFlow::Continue((rest - x, cnt + 1))
+        }
+    });
+
+    let ans = match result {
+        ControlFlow::Break(cnt) if cnt % 3 == 1 => 'F',
+        ControlFlow::Break(cnt) if cnt % 3 == 2 => 'M',
+        ControlFlow::Break(cnt) if cnt % 3 == 0 => 'T',
+        _ => unreachable!(),
+    };
+
+    println!("{ans}");
 }
--- contests/1.70.0/abc243/src/bin/b.rs
+++ contests/1.70.0/abc243/src/bin/b.rs
@@ -1,5 +1,14 @@
+use std::collections::HashSet;
+
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, a: [usize; n], b: [usize; n]);
+
+    let first = a.iter().zip(b.iter()).filter(|(a, b)| a == b).count();
+    let second =
+        a.iter().collect::<HashSet<_>>().intersection(&b.iter().collect::<HashSet<_>>()).count()
+            - first;
+
+    println!("{first}\n{second}");
 }
