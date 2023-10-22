48da4484813e8991cd7583754ae33dbd4d6c93cc
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-22 11:51:35 +0900

    refactor: abc243 a

--- contests/1.70.0/abc243/src/bin/a.rs
+++ contests/1.70.0/abc243/src/bin/a.rs
@@ -1,22 +1,12 @@
-use std::ops::ControlFlow;
-
 use proconio::input;
 
 fn main() {
-    input!(v: isize, abc: [isize; 3]);
+    input!(v: usize, a: usize, b: usize, c: usize);
 
-    let result = abc.iter().cycle().try_fold((v, 0), |(rest, cnt), x| {
-        if rest < 0 {
-            ControlFlow::Break(cnt)
-        } else {
-            ControlFlow::Continue((rest - x, cnt + 1))
-        }
-    });
-
-    let ans = match result {
-        ControlFlow::Break(cnt) if cnt % 3 == 1 => 'F',
-        ControlFlow::Break(cnt) if cnt % 3 == 2 => 'M',
-        ControlFlow::Break(cnt) if cnt % 3 == 0 => 'T',
+    let ans = match v % (a + b + c) {
+        r if r < a => 'F',
+        r if r < a + b => 'M',
+        r if r < a + b + c => 'T',
         _ => unreachable!(),
     };
 
