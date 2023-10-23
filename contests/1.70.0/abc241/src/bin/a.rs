b99c87a72d21002569f121b649288f7eba26696a
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-24 04:08:04 +0900

    feat: abc241 ~b

--- contests/1.70.0/abc241/src/bin/a.rs
+++ contests/1.70.0/abc241/src/bin/a.rs
@@ -1,5 +1,9 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(a: [usize; 10]);
+
+    let ans = a[a[a[0]]];
+
+    println!("{ans}");
 }
--- contests/1.70.0/abc241/src/bin/b.rs
+++ contests/1.70.0/abc241/src/bin/b.rs
@@ -1,5 +1,20 @@
+use hashbag::HashBag;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, m: usize, a: [usize; n], b: [usize; m]);
+
+    let mut bag = a.iter().collect::<HashBag<_>>();
+    let cond = b
+        .iter()
+        .try_for_each(|b| match bag.get(b) {
+            Some(_) => {
+                bag.remove(b);
+                Ok(())
+            },
+            None => Err(()),
+        })
+        .is_ok();
+
+    println!("{}", if cond { "Yes" } else { "No" });
 }
