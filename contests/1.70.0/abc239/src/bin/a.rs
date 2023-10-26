87493f2764aa869de2503129370bf183e70a8e94
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-26 18:27:02 +0900

    feat: abc239 ~b

--- contests/1.70.0/abc239/src/bin/a.rs
+++ contests/1.70.0/abc239/src/bin/a.rs
@@ -1,5 +1,9 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(h: usize);
+
+    let ans = ((h * (12_800_000 + h)) as f64).sqrt();
+
+    println!("{ans}");
 }
--- contests/1.70.0/abc239/src/bin/b.rs
+++ contests/1.70.0/abc239/src/bin/b.rs
@@ -1,5 +1,9 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(x: isize);
+
+    let ans = if x.is_negative() { (x - 9) / 10 } else { x / 10 };
+
+    println!("{ans}");
 }
