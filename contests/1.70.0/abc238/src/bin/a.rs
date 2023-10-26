02cc716baf59e12f4e022bed38a1a36b073caca0
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-27 02:07:54 +0900

    feat: abc238 ~b

--- contests/1.70.0/abc238/src/bin/a.rs
+++ contests/1.70.0/abc238/src/bin/a.rs
@@ -1,5 +1,9 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize);
+
+    let cond = !(2..5).contains(&n);
+
+    println!("{}", if cond { "Yes" } else { "No" });
 }
--- contests/1.70.0/abc238/src/bin/b.rs
+++ contests/1.70.0/abc238/src/bin/b.rs
@@ -1,5 +1,19 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, a: [usize; n]);
+
+    let cutted = a
+        .iter()
+        .scan(0, |acc, a| {
+            *acc = (*acc + a) % 360;
+            Some(*acc)
+        })
+        .chain([0, 360])
+        .sorted();
+
+    let ans = cutted.tuple_windows().map(|(l, r)| r - l).max().unwrap();
+
+    println!("{ans}");
 }
