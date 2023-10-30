60847f2d37d05d53827e0eb8e8e5e400527a688e
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-30 13:30:09 +0900

    feat: 正則でない行列に対応

--- snippets/src/matrix.rs
+++ snippets/src/matrix.rs
@@ -1,14 +1,13 @@
 use cargo_snippet::snippet;
 
 #[snippet(name = ";matrix_rotate")]
-// TODO: 正則でない行列に対応
-fn rotate<T: Copy>(matrix: Vec<Vec<T>>, times: usize) -> Vec<Vec<T>> {
-    let n = matrix.len();
-    assert!(matrix.iter().all(|row| row.len() == n));
+fn rotate<T: Copy>(matrix: &Vec<Vec<T>>, times: usize) -> Vec<Vec<T>> {
+    let (n, m) = (matrix.len(), matrix[0].len());
+    assert!(matrix.iter().map(|row| row.len()).all(|x| x == m));
     match times {
-        0 => matrix,
+        0 => matrix.clone(),
         _ => rotate(
-            (0..n).map(|i| (0..n).map(|j| matrix[n - 1 - j][i]).collect()).collect(),
+            &(0..m).map(|i| (0..n).map(|j| matrix[n - 1 - j][i]).collect()).collect(),
             times - 1,
         ),
     }
@@ -17,11 +16,20 @@ fn rotate<T: Copy>(matrix: Vec<Vec<T>>, times: usize) -> Vec<Vec<T>> {
 #[test]
 fn test_rotate() {
     let matrix = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
-    assert_eq!(rotate(matrix.clone(), 0), vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9],]);
-    assert_eq!(rotate(matrix.clone(), 1), vec![vec![7, 4, 1], vec![8, 5, 2], vec![9, 6, 3],]);
-    assert_eq!(rotate(matrix.clone(), 2), vec![vec![9, 8, 7], vec![6, 5, 4], vec![3, 2, 1],]);
-    assert_eq!(rotate(matrix.clone(), 3), vec![vec![3, 6, 9], vec![2, 5, 8], vec![1, 4, 7],]);
-    assert_eq!(rotate(matrix, 4), vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9],]);
+    assert_eq!(rotate(&matrix, 0), vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9],]);
+    assert_eq!(rotate(&matrix, 1), vec![vec![7, 4, 1], vec![8, 5, 2], vec![9, 6, 3],]);
+    assert_eq!(rotate(&matrix, 2), vec![vec![9, 8, 7], vec![6, 5, 4], vec![3, 2, 1],]);
+    assert_eq!(rotate(&matrix, 3), vec![vec![3, 6, 9], vec![2, 5, 8], vec![1, 4, 7],]);
+    assert_eq!(rotate(&matrix, 4), vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9],]);
+
+    let matrix = vec![vec![1, 2], vec![3, 4]];
+    assert_eq!(rotate(&matrix, 1), vec![vec![3, 1], vec![4, 2],]);
+
+    let matrix = vec![vec![1, 2, 3], vec![4, 5, 6]];
+    assert_eq!(rotate(&matrix, 1), vec![vec![4, 1], vec![5, 2], vec![6, 3],]);
+
+    let matrix = vec![vec![1, 2], vec![3, 4], vec![5, 6]];
+    assert_eq!(rotate(&matrix, 1), vec![vec![5, 3, 1], vec![6, 4, 2],]);
 }
 
 #[snippet(name = ";matrix_transpose")]
