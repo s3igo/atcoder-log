cba63278c91b6f0d309f1d08cc6501ae87843e0a
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-30 13:36:50 +0900

    feat: abc237 ~b

--- contests/1.70.0/abc237/src/bin/a.rs
+++ contests/1.70.0/abc237/src/bin/a.rs
@@ -1,5 +1,10 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: isize);
+
+    let part = 2_isize.pow(31);
+    let cond = (-part..part).contains(&n);
+
+    println!("{}", if cond { "Yes" } else { "No" });
 }
--- contests/1.70.0/abc237/src/bin/b.rs
+++ contests/1.70.0/abc237/src/bin/b.rs
@@ -1,5 +1,16 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(h: usize, w: usize, a: [[usize; w]; h]);
+
+    let b = transpose(&a);
+
+    println!("{}", b.iter().map(|row| row.iter().join(" ")).join("\n"));
+}
+
+fn transpose<T: Copy>(matrix: &Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let (n, m) = (matrix.len(), matrix[0].len());
+    assert!(matrix.iter().map(|row| row.len()).all(|x| x == m));
+    (0..m).map(|i| (0..n).map(|j| matrix[j][i]).collect()).collect()
 }
