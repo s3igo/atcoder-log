39616a3744929e7ce30694f3f30fbf5db6a5c5c9
s3igo <85787242+s3igo@users.noreply.github.com>
2023-10-30 12:31:03 +0900

    feat: update snippets

--- snippets/src/matrix.rs
+++ snippets/src/matrix.rs
@@ -23,3 +23,18 @@ fn test_rotate() {
     assert_eq!(rotate(matrix.clone(), 3), vec![vec![3, 6, 9], vec![2, 5, 8], vec![1, 4, 7],]);
     assert_eq!(rotate(matrix, 4), vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9],]);
 }
+
+#[snippet(name = ";matrix_transpose")]
+fn transpose<T: Copy>(matrix: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let n = matrix.len();
+    assert!(matrix.iter().all(|row| row.len() == n));
+    (0..n).map(|i| (0..n).map(|j| matrix[j][i]).collect()).collect()
+}
+
+#[test]
+fn test_transpose() {
+    let matrix = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+    assert_eq!(transpose(matrix), vec![vec![1, 4, 7], vec![2, 5, 8], vec![3, 6, 9],]);
+    let matrix = vec![vec![1, 2], vec![3, 4]];
+    assert_eq!(transpose(matrix), vec![vec![1, 3], vec![2, 4],]);
+}
