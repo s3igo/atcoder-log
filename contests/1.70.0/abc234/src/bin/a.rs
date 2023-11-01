022b71477dcdeba38f349bac93317eb3ca544280
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-02 04:58:33 +0900

    feat: abc234 ~b

--- contests/1.70.0/abc234/src/bin/a.rs
+++ contests/1.70.0/abc234/src/bin/a.rs
@@ -1,5 +1,10 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(t: usize);
+
+    let f = |x| x * x + 2 * x + 3;
+    let ans = f(f(f(t) + t) + f(f(t)));
+
+    println!("{ans}");
 }
--- contests/1.70.0/abc234/src/bin/b.rs
+++ contests/1.70.0/abc234/src/bin/b.rs
@@ -1,5 +1,18 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, xy: [(isize, isize); n]);
+
+    let ans = xy
+        .iter()
+        .tuple_combinations()
+        .map(|(&a, &b)| euclidean_dst(a, b))
+        .fold(0.0, |acc, x| x.max(acc));
+
+    println!("{ans}");
+}
+
+fn euclidean_dst((ax, ay): (isize, isize), (bx, by): (isize, isize)) -> f64 {
+    (((ax - bx).pow(2) + (ay - by).pow(2)) as f64).sqrt()
 }
