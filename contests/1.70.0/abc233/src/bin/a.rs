a2ccd3a0e1c0a7b5cedf24897e4cdd9304935ac9
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-03 02:29:13 +0900

    feat: abc233 ~b

--- contests/1.70.0/abc233/src/bin/a.rs
+++ contests/1.70.0/abc233/src/bin/a.rs
@@ -1,5 +1,9 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(x: usize, y: usize);
+
+    let ans = (0..).find(|i| x + 10 * i >= y).unwrap();
+
+    println!("{ans}");
 }
--- contests/1.70.0/abc233/src/bin/b.rs
+++ contests/1.70.0/abc233/src/bin/b.rs
@@ -1,5 +1,12 @@
-use proconio::input;
+use proconio::{
+    input,
+    marker::{Chars, Usize1},
+};
 
 fn main() {
-    input!();
+    input!(l: Usize1, r: Usize1, mut s: Chars);
+
+    s[l..=r].reverse();
+
+    println!("{}", s.iter().collect::<String>());
 }
