5a0f21b64c4883450dba6150f83330f925de3ea6
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-05 17:23:18 +0900

    refactor: redundant code removed

--- contests/1.70.0/abc232/src/bin/b.rs
+++ contests/1.70.0/abc232/src/bin/b.rs
@@ -3,17 +3,7 @@ use proconio::input;
 fn main() {
     input!(s: String, t: String);
 
-    let cond = (0..=26).any(|i| rot(&s, i) == t);
+    let cond = (0..=26).any(|i| s.bytes().map(|c| b'a' + (c - b'a' + i) % 26).eq(t.bytes()));
 
     println!("{}", if cond { "Yes" } else { "No" });
-}
-
-fn rot(s: &str, offset: u8) -> String {
-    s.bytes()
-        .map(|c| match c {
-            b'a'..=b'z' => b'a' + (c - b'a' + offset) % 26,
-            b'A'..=b'Z' => b'A' + (c - b'A' + offset) % 26,
-            _ => c,
-        } as char)
-        .collect()
 }
