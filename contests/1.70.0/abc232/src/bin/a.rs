103ac0a56b53651325b0592db2f87e48f19cd9a9
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-05 17:01:11 +0900

    feat: abc232 ~b

--- contests/1.70.0/abc232/src/bin/a.rs
+++ contests/1.70.0/abc232/src/bin/a.rs
@@ -1,5 +1,9 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input!();
+    input!(s: Chars);
+
+    let ans = s[0].to_digit(10).unwrap() * s[2].to_digit(10).unwrap();
+
+    println!("{ans}");
 }
--- contests/1.70.0/abc232/src/bin/b.rs
+++ contests/1.70.0/abc232/src/bin/b.rs
@@ -1,5 +1,19 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(s: String, t: String);
+
+    let cond = (0..=26).any(|i| rot(&s, i) == t);
+
+    println!("{}", if cond { "Yes" } else { "No" });
+}
+
+fn rot(s: &str, offset: u8) -> String {
+    s.bytes()
+        .map(|c| match c {
+            b'a'..=b'z' => b'a' + (c - b'a' + offset) % 26,
+            b'A'..=b'Z' => b'A' + (c - b'A' + offset) % 26,
+            _ => c,
+        } as char)
+        .collect()
 }
