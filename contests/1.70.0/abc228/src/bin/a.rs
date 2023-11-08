3144205825431e63754a56d600b57fda044e7f1e
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-09 03:26:01 +0900

    feat: abc228 ~b

--- contests/1.70.0/abc228/src/bin/a.rs
+++ contests/1.70.0/abc228/src/bin/a.rs
@@ -1,5 +1,9 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(s: usize, t: usize, x: usize);
+
+    let cond = if s < t { (s..t).contains(&x) } else { s <= x || x < t };
+
+    println!("{}", if cond { "Yes" } else { "No" });
 }
--- contests/1.70.0/abc228/src/bin/b.rs
+++ contests/1.70.0/abc228/src/bin/b.rs
@@ -1,5 +1,18 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
-    input!();
+    input!(n: usize, x: Usize1, a: [Usize1; n]);
+
+    let ans = trace(&a, &mut vec![false; n], x, 0);
+
+    println!("{ans}");
+}
+
+fn trace(a: &[usize], checked: &mut [bool], x: usize, cnt: usize) -> usize {
+    if checked[x] {
+        cnt
+    } else {
+        checked[x] = true;
+        trace(a, checked, a[x], cnt + 1)
+    }
 }
