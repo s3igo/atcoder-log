798dad5bc2be0580c084391d4a371058e2f87b52
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-10 13:38:56 +0900

    refactor: remove redundant code

--- contests/1.70.0/abc227/src/bin/b.rs
+++ contests/1.70.0/abc227/src/bin/b.rs
@@ -6,11 +6,11 @@ use proconio::input;
 fn main() {
     input!(n: usize, s: [i32; n]);
 
-    let f = |(i, j)| 3 * i + 4 * i * j + 3 * j;
-
     let results = iproduct!(1..=1000, 1..=1000)
-        .filter(|&(a, b)| f((a, b)) <= 1000)
-        .map(f)
+        .filter_map(|(a, b)| match 3 * a + 4 * a * b + 3 * b {
+            area @ ..=1000 => Some(area),
+            _ => None,
+        })
         .collect::<HashSet<_>>();
 
     let ans = s.iter().filter(|&s| !results.contains(s)).count();
