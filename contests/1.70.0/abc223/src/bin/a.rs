3a57e1c51876bcee13677eb1cbf35d3db78ebb90
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-15 12:02:01 +0900

    feat: add abc223 ~b

--- contests/1.70.0/abc223/src/bin/a.rs
+++ contests/1.70.0/abc223/src/bin/a.rs
@@ -1,5 +1,9 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(x: usize);
+
+    let ans = if x == 0 || x % 100 != 0 { "No" } else { "Yes" };
+
+    println!("{}", ans);
 }
--- contests/1.70.0/abc223/src/bin/b.rs
+++ contests/1.70.0/abc223/src/bin/b.rs
@@ -1,5 +1,24 @@
+use std::collections::VecDeque;
+
+use itertools::{Itertools, MinMaxResult};
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(s: String);
+
+    let minmax = (0..s.len())
+        .map(|i| {
+            let mut s = s.chars().collect::<VecDeque<_>>();
+            s.rotate_left(i);
+            s
+        })
+        .minmax();
+
+    let (min, max): (String, String) = match minmax {
+        MinMaxResult::OneElement(s) => (s.iter().collect(), s.iter().collect()),
+        MinMaxResult::MinMax(min, max) => (min.iter().collect(), max.iter().collect()),
+        _ => unreachable!(),
+    };
+
+    println!("{min}\n{max}");
 }
