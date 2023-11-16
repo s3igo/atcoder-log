ccc8c2b5e11686b2cee500839cc34acb3ed46c76
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-16 10:38:36 +0900

    feat: abc222 ~b

--- contests/1.70.0/abc222/src/bin/b.rs
+++ contests/1.70.0/abc222/src/bin/b.rs
@@ -1,5 +1,9 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, p: usize, a: [usize; n]);
+
+    let ans = a.iter().filter(|&&x| x < p).count();
+
+    println!("{}", ans);
 }
