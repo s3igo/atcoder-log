f83d15d13d2b8254fa166bc302efb6df34081d31
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-19 12:15:36 +0900

    refactor: abc329

--- contests/1.70.0/abc329/src/bin/d.rs
+++ contests/1.70.0/abc329/src/bin/d.rs
@@ -1,18 +1,20 @@
-use std::collections::BTreeSet;
+use std::cmp::Ordering;
 
 use proconio::{input, marker::Usize1};
 
 fn main() {
     input!(n: usize, m: usize, a: [Usize1; m]);
 
-    let mut set = std::iter::repeat(m).zip(0..n).collect::<BTreeSet<_>>();
-    let mut cnt = vec![m; n];
-
+    let (mut cnt, mut prev) = (vec![0; n], 0);
     for a in a {
-        set.remove(&(cnt[a], a));
-        cnt[a] -= 1;
-        set.insert((cnt[a], a));
-        let ans = set.first().unwrap().1 + 1;
-        println!("{ans}");
+        cnt[a] += 1;
+
+        prev = match cnt[prev].cmp(&cnt[a]) {
+            Ordering::Less => a,
+            Ordering::Greater => prev,
+            Ordering::Equal => prev.min(a),
+        };
+
+        println!("{}", prev + 1);
     }
 }
