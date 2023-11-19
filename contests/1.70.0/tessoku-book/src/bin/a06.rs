8079dd4d366040f045ff66169a13ef962bbfc685
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-19 16:18:05 +0900

    feat: a06

--- contests/1.70.0/tessoku-book/src/bin/a06.rs
+++ contests/1.70.0/tessoku-book/src/bin/a06.rs
@@ -1,5 +1,12 @@
+use itertools_num::ItertoolsNum;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, q: usize, a: [usize; n], lr: [(usize, usize); q]);
+
+    let cumsum = std::iter::once(0).chain(a).cumsum().collect::<Vec<usize>>();
+
+    for (l, r) in lr {
+        println!("{}", cumsum[r] - cumsum[l - 1]);
+    }
 }
