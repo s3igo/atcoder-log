d930e77add93d53dc9591f340d8e7a890f7a4160
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-21 08:30:44 +0900

    feat: a14

--- contests/1.70.0/tessoku-book/src/bin/a14.rs
+++ contests/1.70.0/tessoku-book/src/bin/a14.rs
@@ -1,5 +1,11 @@
+use itertools::{iproduct, Itertools};
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, k: usize, a: [usize; n], b: [usize; n], c: [usize; n], d: [usize; n]);
+
+    let q = iproduct!(c, d).map(|(c, d)| c + d).sorted().collect::<Vec<_>>();
+    let cond = iproduct!(a, b).map(|(a, b)| a + b).any(|p| q.binary_search(&(k - p)).is_ok());
+
+    println!("{}", if cond { "Yes" } else { "No" });
 }
