7150f18d949e07471b933d0b996b0659af066aee
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-20 14:14:48 +0900

    refactor: two-pointers

--- contests/1.70.0/tessoku-book/src/bin/a13.rs
+++ contests/1.70.0/tessoku-book/src/bin/a13.rs
@@ -3,14 +3,10 @@ use proconio::input;
 fn main() {
     input!(n: usize, k: usize, a: [usize; n]);
 
-    let (mut l, mut r, mut cnt) = (0, 0, 0);
-    while l < n - 1 {
-        while r < n - 1 && a[r + 1] - a[l] <= k {
-            r += 1;
-        }
-        cnt += r - l;
-        l += 1;
-    }
+    let (ans, _) = (0..n).fold((0, 0), |(acc, rhs), lhs| {
+        let rhs = (rhs..n).find(|&rhs| a[rhs] - a[lhs] > k).unwrap_or(n);
+        (acc + rhs - lhs - 1, rhs)
+    });
 
-    println!("{cnt}");
+    println!("{ans}");
 }
