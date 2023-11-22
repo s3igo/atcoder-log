495f256e8d56e6f76b0aa0d84b2d04076102e048
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-22 14:26:21 +0900

    feat: b14

--- contests/1.70.0/tessoku-book/src/bin/b14.rs
+++ contests/1.70.0/tessoku-book/src/bin/b14.rs
@@ -1,5 +1,19 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, k: usize, a: [usize; n]);
+
+    let (lhs, rhs) = a.split_at(n / 2);
+
+    let rhs =
+        rhs.iter().powerset().map(|x| x.into_iter().sum::<usize>()).sorted().collect::<Vec<_>>();
+
+    let cond = lhs
+        .iter()
+        .powerset()
+        .map(|x| x.into_iter().sum::<usize>())
+        .any(|x| rhs.binary_search(&(k - x)).is_ok());
+
+    println!("{}", if cond { "Yes" } else { "No" });
 }
