dcc322c1507acc0490dea13dd947090516b6f73d
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-23 01:44:17 +0900

    feat: a15

--- contests/1.70.0/tessoku-book/src/bin/a15.rs
+++ contests/1.70.0/tessoku-book/src/bin/a15.rs
@@ -1,5 +1,11 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, a: [usize; n]);
+
+    let asc = a.iter().sorted().dedup().collect::<Vec<_>>();
+    let ans = a.iter().map(|a| asc.binary_search(&a).unwrap() + 1).collect::<Vec<_>>();
+
+    println!("{}", ans.iter().join(" "));
 }
