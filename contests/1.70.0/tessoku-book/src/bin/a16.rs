f4b4a58d3ea6fde0e933f82e9d9e9ad07781da62
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-24 19:12:59 +0900

    feat: a16

--- contests/1.70.0/tessoku-book/src/bin/a16.rs
+++ contests/1.70.0/tessoku-book/src/bin/a16.rs
@@ -1,5 +1,18 @@
+use memoise::memoise;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, a: [usize; n - 1], b: [usize; n - 2]);
+
+    println!("{}", dp(n, &a, &b));
+}
+
+#[memoise(n)]
+fn dp(n: usize, a: &[usize], b: &[usize]) -> usize {
+    let dp = |x| dp(x, a, b);
+    match n {
+        1 => 0,
+        2 => a[0],
+        _ => (dp(n - 1) + a[n - 2]).min(dp(n - 2) + b[n - 3]),
+    }
 }
