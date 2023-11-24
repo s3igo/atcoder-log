db1c04a51281ba70630203fa99a310aa3b30941c
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-24 20:58:59 +0900

    feat: b16

--- contests/1.70.0/tessoku-book/src/bin/b16.rs
+++ contests/1.70.0/tessoku-book/src/bin/b16.rs
@@ -1,5 +1,22 @@
+use memoise::memoise;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, h: [usize; n]);
+
+    println!("{}", dp(n, &h));
+}
+
+#[memoise(n)]
+fn dp(n: usize, h: &[usize]) -> usize {
+    let dp = |x| dp(x, h);
+    match n {
+        1 => 0,
+        2 => h[1].abs_diff(h[0]),
+        _ => {
+            let short = dp(n - 1) + h[n - 1].abs_diff(h[n - 2]);
+            let long = dp(n - 2) + h[n - 1].abs_diff(h[n - 3]);
+            short.min(long)
+        },
+    }
 }
