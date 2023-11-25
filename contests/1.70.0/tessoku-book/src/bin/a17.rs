ee035313c91cbb5dd28ea4cefcbeeb1127221f9d
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-25 13:00:54 +0900

    feat: a17

--- contests/1.70.0/tessoku-book/src/bin/a17.rs
+++ contests/1.70.0/tessoku-book/src/bin/a17.rs
@@ -1,5 +1,28 @@
+use itertools::Itertools;
+use memoise::memoise;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, a: [usize; n - 1], b: [usize; n - 2]);
+
+    let dp = |x| dp(x, &a, &b);
+
+    let ans: Vec<_> = std::iter::successors(Some(n), |&acc| match acc {
+        p @ 2.. if dp(p - 1) + a[p - 2] == dp(p) => Some(p - 1),
+        p @ 3.. if dp(p - 2) + b[p - 3] == dp(p) => Some(p - 2),
+        _ => None,
+    })
+    .collect();
+
+    println!("{}\n{}", ans.len(), ans.iter().rev().join(" "));
+}
+
+#[memoise(n)]
+fn dp(n: usize, a: &[usize], b: &[usize]) -> usize {
+    let dp = |x| dp(x, a, b);
+    match n {
+        1 => 0,
+        2 => a[0],
+        _ => (dp(n - 1) + a[n - 2]).min(dp(n - 2) + b[n - 3]),
+    }
 }
