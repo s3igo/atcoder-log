9de2cdb2428a4dff4d0338226db1639c8e855f30
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-26 19:17:42 +0900

    feat: a18

--- contests/1.70.0/tessoku-book/src/bin/a18.rs
+++ contests/1.70.0/tessoku-book/src/bin/a18.rs
@@ -1,5 +1,18 @@
+use memoise::memoise;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, s: usize, a: [usize; n]);
+
+    println!("{}", if dp(n, s, &a) { "Yes" } else { "No" });
+}
+
+#[memoise(i, j)]
+fn dp(i: usize, j: usize, a: &[usize]) -> bool {
+    let dp = |x, y| dp(x, y, a);
+    match (i, j) {
+        (0, 0) => true,
+        (0, _) => false,
+        _ => dp(i - 1, j) || j.checked_sub(a[i - 1]).map_or(false, |x| dp(i - 1, x)),
+    }
 }
