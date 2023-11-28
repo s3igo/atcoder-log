0a3f38acf899e41da2d68a3f8de9a14b7c768b7b
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-28 12:17:33 +0900

    feat: a19

--- contests/1.70.0/tessoku-book/src/bin/a19.rs
+++ contests/1.70.0/tessoku-book/src/bin/a19.rs
@@ -1,5 +1,20 @@
+use memoise::memoise;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, w: usize, wv: [(usize, usize); n]);
+
+    println!("{}", dp(n, w, &wv));
+}
+
+#[memoise(i, j)]
+fn dp(i: usize, j: usize, wv: &[(usize, usize)]) -> usize {
+    let dp = |x, y| dp(x, y, wv);
+    match (i, j) {
+        (0, _) => 0,
+        _ => match wv[i - 1] {
+            (w, v) if j >= w => dp(i - 1, j).max(dp(i - 1, j - w) + v),
+            _ => dp(i - 1, j),
+        },
+    }
 }
