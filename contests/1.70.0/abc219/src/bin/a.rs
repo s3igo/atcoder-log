907b85601d10cb61a6d501610442dd0df149687b
s3igo <85787242+s3igo@users.noreply.github.com>
2023-11-30 18:37:44 +0900

    feat: abc219 ~b

--- contests/1.70.0/abc219/src/bin/a.rs
+++ contests/1.70.0/abc219/src/bin/a.rs
@@ -1,5 +1,14 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(x: usize);
+
+    let ans = match x {
+        0..=39 => (40 - x).to_string(),
+        40..=69 => (70 - x).to_string(),
+        70..=89 => (90 - x).to_string(),
+        _ => "expert".to_string(),
+    };
+
+    println!("{ans}");
 }
--- contests/1.70.0/abc219/src/bin/b.rs
+++ contests/1.70.0/abc219/src/bin/b.rs
@@ -1,5 +1,17 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(s1: String, s2: String, s3: String, t: String);
+
+    let ans: String = t
+        .chars()
+        .map(|c| match c {
+            '1' => s1.clone(),
+            '2' => s2.clone(),
+            '3' => s3.clone(),
+            _ => unreachable!(),
+        })
+        .collect();
+
+    println!("{ans}");
 }
