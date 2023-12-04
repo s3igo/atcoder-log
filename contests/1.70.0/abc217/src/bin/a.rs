81c68d4375a0d20d9c89ab56dd4e44aecb35f9fe
s3igo <85787242+s3igo@users.noreply.github.com>
2023-12-04 19:45:24 +0900

    feat: add abc217 ~b

--- contests/1.70.0/abc217/src/bin/a.rs
+++ contests/1.70.0/abc217/src/bin/a.rs
@@ -1,5 +1,7 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(s: String, t: String);
+
+    println!("{}", if s < t { "Yes" } else { "No" });
 }
--- contests/1.70.0/abc217/src/bin/b.rs
+++ contests/1.70.0/abc217/src/bin/b.rs
@@ -1,5 +1,10 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(s: [String; 3]);
+
+    let cases = ["ABC", "ARC", "AGC", "AHC"];
+    let ans = cases.iter().find(|c| !s.contains(&c.to_string())).unwrap();
+
+    println!("{ans}");
 }
