bbd1b8102ee5c329633b4853f28dae10c3cab431
s3igo <85787242+s3igo@users.noreply.github.com>
2023-12-06 15:12:17 +0900

    feat: add b20

--- contests/1.70.0/tessoku-book/src/bin/b20.rs
+++ contests/1.70.0/tessoku-book/src/bin/b20.rs
@@ -1,5 +1,20 @@
-use proconio::input;
+use memoise::memoise;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input!();
+    input!(s: Chars, t: Chars);
+
+    println!("{}", dp(s.len(), t.len(), &s, &t));
+}
+
+#[memoise(i, j)]
+fn dp(i: usize, j: usize, s: &[char], t: &[char]) -> usize {
+    let dp = |x, y| dp(x, y, s, t);
+    match (i, j) {
+        (0, 0) => 0,
+        (0, _) => j,
+        (_, 0) => i,
+        (i, j) if s[i - 1] == t[j - 1] => dp(i - 1, j - 1),
+        (i, j) => dp(i - 1, j).min(dp(i, j - 1)).min(dp(i - 1, j - 1)) + 1,
+    }
 }
