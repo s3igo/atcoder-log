b4828b1b22f5a7e42868684e61be95d254995ef9
s3igo <85787242+s3igo@users.noreply.github.com>
2023-12-09 09:55:58 +0900

    feat: a22

--- contests/1.70.0/tessoku-book/src/bin/a22.rs
+++ contests/1.70.0/tessoku-book/src/bin/a22.rs
@@ -1,5 +1,17 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
-    input!();
+    input!(n: usize, a: [Usize1; n - 1], b: [Usize1; n - 1]);
+
+    let mut dp = vec![None; n];
+    dp[0] = Some(0);
+    for i in 0..n - 1 {
+        let (a, b) = (a[i], b[i]);
+        if let Some(cur) = dp[i] {
+            dp[a] = Some(dp[a].map_or(cur + 100, |x| x.max(cur + 100)));
+            dp[b] = Some(dp[b].map_or(cur + 150, |x| x.max(cur + 150)));
+        }
+    }
+
+    println!("{}", dp[n - 1].unwrap());
 }
