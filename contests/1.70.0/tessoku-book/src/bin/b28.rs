0676ec22b39b719bad87825ff186351f6632dd6a
s3igo <85787242+s3igo@users.noreply.github.com>
2023-12-15 15:14:44 +0900

    feat: b28

--- contests/1.70.0/tessoku-book/src/bin/b28.rs
+++ contests/1.70.0/tessoku-book/src/bin/b28.rs
@@ -1,5 +1,10 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize);
+    let div = 1_000_000_007;
+
+    let (_, ans) = (3..=n).fold((1, 1), |(prev, cur), _| (cur, (prev + cur) % div));
+
+    println!("{ans}");
 }
