fafdb87928979c84a3585a37f181974dd4653a7e
s3igo <85787242+s3igo@users.noreply.github.com>
2023-12-25 12:20:10 +0900

    feat: abc334 b

--- contests/1.70.0/abc334/src/bin/b.rs
+++ contests/1.70.0/abc334/src/bin/b.rs
@@ -1,16 +1,13 @@
+use num_integer::Integer;
 use proconio::input;
 
 fn main() {
-    input!(a: isize, m: usize, l: isize, r: isize);
+    input!(a: isize, m: isize, l: isize, r: isize);
 
-    // let ans = if a.abs_diff(l) % m == 0 || a.abs_diff(r) % m == 0 {
-    //     (l.abs_diff(r) + m - 1) / m
-    // } else {
-    //     l.abs_diff(r) / m
-    // };
+    let l = l - a;
+    let r = r - a;
 
-    let offset = if a.abs_diff(l) % m == 0 { 0 } else { m - a.abs_diff(l) % m };
-    let ans = (l.abs_diff(r) + m - offset - 1) / m;
+    let ans = Integer::div_floor(&r, &m) - Integer::div_floor(&(l - 1), &m);
 
     println!("{ans}");
 }
