14ed0178ab3b9319590c865eb5c22520ef288ddc
s3igo <85787242+s3igo@users.noreply.github.com>
2023-12-26 18:37:40 +0900

    feat: abc216 c

--- contests/1.70.0/abc216/src/bin/c.rs
+++ contests/1.70.0/abc216/src/bin/c.rs
@@ -1,5 +1,20 @@
+use itertools::Itertools;
+use num_integer::Integer;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(mut n: usize);
+
+    let mut s = vec![];
+    while n != 0 {
+        if n.is_even() {
+            s.push('B');
+            n /= 2;
+        } else {
+            s.push('A');
+            n -= 1;
+        }
+    }
+
+    println!("{}", s.iter().rev().join(""));
 }
