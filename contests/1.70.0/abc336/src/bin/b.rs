f6af232e1f2528f5536cb9f890bf0bbc803ed083
s3igo <85787242+s3igo@users.noreply.github.com>
2024-01-15 00:31:22 +0900

    feat: add abc336

--- contests/1.70.0/abc336/src/bin/b.rs
+++ contests/1.70.0/abc336/src/bin/b.rs
@@ -1,5 +1,7 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize);
+
+    println!("{}", n.trailing_zeros());
 }
--- contests/1.70.0/abc336/src/bin/c.rs
+++ contests/1.70.0/abc336/src/bin/c.rs
@@ -1,5 +1,32 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(mut n: usize);
+
+    let mut nums = vec![];
+    while n > 0 {
+        nums.push(n % 5);
+        n /= 5;
+    }
+
+    let digits: Vec<_> = nums.iter().map(|x| x * 2).collect();
+
+    let digits: Vec<_> = digits
+        .iter()
+        .scan(2, |acc, i| match i.checked_sub(*acc) {
+            Some(n) => {
+                *acc = 0;
+                Some(n)
+            },
+            None => {
+                *acc = 2;
+                Some(8)
+            },
+        })
+        .collect();
+
+    let ans = digits.iter().rev().join("");
+
+    println!("{ans}");
 }
