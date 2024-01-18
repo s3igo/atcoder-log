65e723ebedf383f0e624473c58f1b04b9a428880
s3igo <85787242+s3igo@users.noreply.github.com>
2024-01-18 20:08:36 +0900

    feat: add abc219-c

--- contests/1.70.0/abc219/src/bin/c.rs
+++ contests/1.70.0/abc219/src/bin/c.rs
@@ -1,5 +1,12 @@
+use itertools::Itertools;
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
-    input!();
+    input!(x: String, n: usize, s: [String; n]);
+
+    let ord: HashMap<_, _> = x.chars().zip(0..).collect();
+    let ans: Vec<_> = s.iter().sorted_by_key(|s| s.chars().map(|c| ord[&c]).collect::<Vec<_>>()).collect();
+
+    println!("{}", ans.iter().join("\n"));
 }
