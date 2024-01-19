b5f389817d144d196808241b87691d7a66a91dda
s3igo <85787242+s3igo@users.noreply.github.com>
2024-01-19 11:17:42 +0900

    feat: add abc220-c

--- contests/1.70.0/abc220/src/bin/c.rs
+++ contests/1.70.0/abc220/src/bin/c.rs
@@ -1,5 +1,15 @@
+use itertools::Itertools;
+use itertools_num::ItertoolsNum;
+
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, a: [usize; n], x: usize);
+
+    let sum: usize = a.iter().sum();
+    let cumsum: Vec<_> = a.iter().cumsum::<usize>().collect();
+
+    let ans = x / sum * n + cumsum.iter().take_while_inclusive(|&&c| c <= x % sum).count();
+
+    println!("{ans}");
 }
