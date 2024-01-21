2dc704a9e06c56e94cab556fe265f7e458b3a855
s3igo <85787242+s3igo@users.noreply.github.com>
2024-01-21 18:32:12 +0900

    feat: abc221-c

--- contests/1.70.0/abc221/src/bin/c.rs
+++ contests/1.70.0/abc221/src/bin/c.rs
@@ -1,5 +1,18 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: String);
+
+    let mut ans = 0;
+    for s in n.chars().permutations(n.len()) {
+        for (l, r) in (1..n.len()).map(|i| s.split_at(i)) {
+            let check = |v: &[char]| v.iter().collect::<String>().parse::<usize>();
+            if let (Ok(l), Ok(r)) = (check(l), check(r)) {
+                ans = ans.max(l * r);
+            }
+        }
+    }
+
+    println!("{ans}");
 }
