3a7a12ca17ff8b3aa8b4404354d216fc39ef786a
s3igo <85787242+s3igo@users.noreply.github.com>
2024-01-29 21:25:41 +0900

    feat: add abc223-c

--- contests/1.70.0/abc223/src/bin/c.rs
+++ contests/1.70.0/abc223/src/bin/c.rs
@@ -1,5 +1,10 @@
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, ab: [(f64, f64); n]);
+
+    let t = ab.iter().map(|(a, b)| a / b).sum::<f64>() / 2.;
+    let (ans, _) = ab.iter().fold((0., t), |(l, t), (a, b)| (l + a.min(t * b), t - t.min(a / b)));
+
+    println!("{ans}");
 }
