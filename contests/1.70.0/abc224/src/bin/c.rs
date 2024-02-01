8d8abb700008e068660f0808b41685a2236655e0
s3igo <85787242+s3igo@users.noreply.github.com>
2024-02-01 20:44:34 +0900

    feat: abc224-c

--- contests/1.70.0/abc224/src/bin/c.rs
+++ contests/1.70.0/abc224/src/bin/c.rs
@@ -1,5 +1,14 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!();
+    input!(n: usize, xy: [(isize, isize); n]);
+
+    let ans = xy
+        .iter()
+        .tuple_combinations()
+        .filter(|(a, b, c)| (b.0 - a.0) * (c.1 - a.1) - (b.1 - a.1) * (c.0 - a.0) != 0)
+        .count();
+
+    println!("{ans}");
 }
