7270f9892197c3ef0e3e999ce81da33784b25c8a
s3igo <85787242+s3igo@users.noreply.github.com>
2024-02-06 16:12:07 +0900

    refactor: abc339-b

--- contests/1.70.0/abc339/src/bin/b.rs
+++ contests/1.70.0/abc339/src/bin/b.rs
@@ -2,35 +2,29 @@ use itertools::Itertools;
 use proconio::input;
 
 fn main() {
-    input!(h: isize, w: isize, n: isize);
+    input!(h: usize, w: usize, n: usize);
 
-    let mut grid = vec![vec!['.'; w as usize]; h as usize];
+    let mut grid = vec![vec![false; w]; h];
     let mut direction = 0;
     let mut cur = (0, 0);
     for _ in 0..n {
         let (i, j) = cur;
-        if grid[i as usize][j as usize] == '.' {
-            grid[i as usize][j as usize] = '#';
-            direction = (direction + 1) % 4;
-            cur = move_forward(direction, cur, (h, w));
-        } else {
-            grid[i as usize][j as usize] = '.';
-            direction = (direction + 3) % 4;
-            cur = move_forward(direction, cur, (h, w));
-        }
+        grid[i][j] = !grid[i][j];
+        direction = (direction + if grid[i][j] { 1 } else { 3 }) % 4;
+        cur = move_forward(direction, cur, (h, w));
     }
 
-    println!("{}", grid.iter().map(|row| row.iter().join("")).join("\n"));
+    for row in grid {
+        println!("{}", row.iter().map(|&is| if is { '#' } else { '.' }).join(""));
+    }
 }
 
-fn move_forward(direction: isize, cur: (isize, isize), max: (isize, isize)) -> (isize, isize) {
-    let (i, j) = cur;
-    let (h, w) = max;
+fn move_forward(direction: usize, (i, j): (usize, usize), (h, w): (usize, usize)) -> (usize, usize) {
     match direction {
-        0 => (if i - 1 < 0 { h - 1 } else { i - 1 }, j),
-        1 => (i, if j + 1 == w { 0 } else { j + 1 }),
-        2 => (if i + 1 == h { 0 } else { i + 1 }, j),
-        3 => (i, if j - 1 < 0 { w - 1 } else { j - 1 }),
+        0 => ((i + h - 1) % h, j),
+        1 => (i, (j + 1) % w),
+        2 => ((i + 1) % h, j),
+        3 => (i, (j + w - 1) % w),
         _ => unreachable!(),
     }
 }
