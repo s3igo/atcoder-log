dcd0a10107d0f1bcc548b07aa55ce923d4af7933
s3igo <85787242+s3igo@users.noreply.github.com>
2024-02-06 16:16:37 +0900

    chore: remove unused scripts

--- flake.nix
+++ flake.nix
@@ -26,11 +26,6 @@
         pkgs = import nixpkgs { inherit system overlays; };
         cargo-compete = import ./cargo-compete.nix { inherit pkgs; };
         # cargo-snippet = import ./cargo-snippet.nix { inherit nixpkgs system rust-overlay; };
-        ojt = pkgs.writeShellScriptBin "ojt" ''
-          # $1: bin name (e.g. abc001-a)
-          declare ROOT="$(git rev-parse --show-toplevel)"
-          cargo build --release --bin "$1" && oj t -c "$ROOT/target/release/$1"
-        '';
         new = pkgs.writeShellScriptBin "task_new" ''
           cat <<EOF
           cargo compete new "$1" \
@@ -42,17 +37,15 @@
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = [
-            pkgs.rustToolchain
-            pkgs.rust-analyzer
-            pkgs.statix
-            pkgs.online-judge-tools
+          buildInputs = with pkgs; [
+            rustToolchain
+            rust-analyzer
+            statix
             cargo-compete
-            # cargoAlias
-            ojt
             new
           ];
         };
+
         formatter = pkgs.nixfmt-rfc-style;
       }
     );
