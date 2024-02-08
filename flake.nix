7a9502934578286613ddf2a6c805d4f3dbed879a
s3igo <85787242+s3igo@users.noreply.github.com>
2024-02-08 11:29:47 +0900

    feat: change to use fenix from rust-overlay

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -11,39 +14,52 @@
     {
       self,
       nixpkgs,
-      rust-overlay,
+      fenix,
       flake-utils,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        overlays = [
-          (import rust-overlay)
-          (self: super: {
-            rustToolchain = super.rust-bin.fromRustupToolchainFile ./contests/1.70.0/rust-toolchain.toml;
-          })
-        ];
-        pkgs = import nixpkgs { inherit system overlays; };
+        pkgs = import nixpkgs { inherit system; };
+        toolchain =
+          with fenix.packages.${system};
+          combine [
+            (fromToolchainFile {
+              file = ./contests/1.70.0/rust-toolchain.toml;
+              sha256 = "sha256-gdYqng0y9iHYzYPAdkC/ka3DRny3La/S5G8ASj0Ayyc=";
+            })
+            # nightly versions of rust-analyzer and rustfmt
+            rust-analyzer
+            default.rustfmt
+          ];
         cargo-compete = import ./cargo-compete.nix { inherit pkgs; };
-        # cargo-snippet = import ./cargo-snippet.nix { inherit nixpkgs system rust-overlay; };
-        new = pkgs.writeShellScriptBin "task_new" ''
-          cat <<EOF
-          cargo compete new "$1" \
-              && git add "$1" \
-              && git commit -m "feat: add $1" \
-              && cd "$1"
-          EOF
-        '';
+        # cargo-snippet = import ./cargo-snippet.nix { inherit pkgs fenix; };
+        tasks =
+          let
+            new = pkgs.writeScriptBin "task_new" ''
+              cat <<EOF
+              cargo compete new "$1" \
+                  && git add "$1" \
+                  && git commit -m "feat: add $1" \
+                  && cd "$1"
+              EOF
+            '';
+          in
+          [ new ];
       in
       {
+        # packages.default = cargo-snippet;
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            rustToolchain
-            rust-analyzer
-            statix
-            cargo-compete
-            new
-          ];
+          buildInputs =
+            let
+              deps = [
+                toolchain
+                cargo-compete
+                # cargo-snippet
+              ];
+            in
+            with pkgs;
+            [ statix ] ++ deps ++ tasks;
         };
 
         formatter = pkgs.nixfmt-rfc-style;
