25b4c2e993355cfd3f61e5b3333d33ba0487a1d9
s3igo <85787242+s3igo@users.noreply.github.com>
2024-03-05 18:42:43 +0900

    feat: udpate flake

--- flake.nix
+++ flake.nix
@@ -1,23 +1,16 @@
 {
-  description = "AtCoder development environment";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     dotfiles.url = "github:s3igo/dotfiles";
   };
 
   outputs =
     {
-      self,
       nixpkgs,
-      nixvim,
-      dotfiles,
       flake-utils,
+      dotfiles,
+      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -27,20 +20,29 @@
       in
       {
         packages = {
-          neovim = nixvim.legacyPackages.${system}.makeNixvimWithModule {
-            module = {
-              imports = [ dotfiles.nixosModules.neovim ];
-
-              plugins.lsp.servers.rust-analyzer = {
-                enable = true;
-                installCargo = false;
-                installRustc = false;
-                settings = {
-                  check.command = "clippy";
-                  files.excludeDirs = [ ".direnv" ];
+          neovim = dotfiles.neovim.${system} {
+            inherit pkgs;
+            modules = [
+              dotfiles.nixosModules.nix
+              (_: {
+                plugins = {
+                  treesitter.grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+                    rust
+                    toml
+                    regex
+                  ];
+                  lsp.servers.rust-analyzer = {
+                    enable = true;
+                    installCargo = false;
+                    installRustc = false;
+                    settings = {
+                      check.command = "clippy";
+                      files.excludeDirs = [ ".direnv" ];
+                    };
+                  };
                 };
-              };
-            };
+              })
+            ];
           };
         };
 
