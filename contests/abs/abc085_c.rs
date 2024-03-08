0ccff1d3585b3d175103256c98d62cded0d7e393
s3igo <85787242+s3igo@users.noreply.github.com>
2024-03-08 16:46:34 +0900

    refactor: abs

--- contests/abs/abc085_c.rs
+++ contests/abs/abc085_c.rs
@@ -4,7 +4,7 @@ fn main() {
     input!(n: usize, y: usize);
 
     let ans = (0..=n)
-        .flat_map(move |i| (0..=(n - i)).map(move |j| (i, j, n - i - j)))
+        .flat_map(move |i| (0..=n - i).map(move |j| (i, j, n - i - j)))
         .find(|(i, j, k)| 10000 * i + 5000 * j + 1000 * k == y)
         .map_or("-1 -1 -1".to_string(), |(a, b, c)| format!("{a} {b} {c}"));
 
--- contests/abs/abc086_c.rs
+++ contests/abs/abc086_c.rs
@@ -5,10 +5,10 @@ fn main() {
 
     let cond = txy
         .into_iter()
-        .try_fold((0, 0, 0), |(pt, px, py), (t, x, y)| {
+        .try_fold((0, 0_usize, 0_usize), |(pt, px, py), (t, x, y)| {
             let dt = t - pt;
-            let dst = manhattan_dst((px, py), (x, y));
-            if dt < dst || dt % 2 != dst % 2 {
+            let manhattan_dst = px.abs_diff(x) + py.abs_diff(y);
+            if dt < manhattan_dst || dt % 2 != manhattan_dst % 2 {
                 None
             } else {
                 Some((t, x, y))
@@ -17,8 +17,4 @@ fn main() {
         .is_some();
 
     println!("{}", if cond { "Yes" } else { "No" })
-}
-
-fn manhattan_dst((ax, ay): (usize, usize), (bx, by): (usize, usize)) -> usize {
-    ax.abs_diff(bx) + ay.abs_diff(by)
 }
