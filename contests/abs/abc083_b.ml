7c8678f9bdd528f8d4f46b578dad70658356c843
s3igo <85787242+s3igo@users.noreply.github.com>
2024-04-29 08:40:12 +0900

    refactor: abs

--- contests/abs/abc083_b.ml
+++ contests/abs/abc083_b.ml
@@ -2,11 +2,9 @@ open Core
 open Scanf
 
 let contains l r x =
-  let sum =
-    let chars = string_of_int x |> String.to_list in
-    List.(chars >>| String.make 1 |> sum (module Int) ~f:int_of_string)
-  in
-  l <= sum && sum <= r
+  let chars = string_of_int x |> String.to_list in
+  List.(chars >>| String.make 1 |> sum (module Int) ~f:int_of_string)
+  |> fun sum -> l <= sum && sum <= r
 
 let () =
   scanf "%d %d %d" @@ fun n a b ->
