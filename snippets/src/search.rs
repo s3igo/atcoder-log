69362907ef9140b5e4c8f0853c2c9b6b727a0d07
s3igo <85787242+s3igo@users.noreply.github.com>
2024-05-03 18:16:09 +0900

    fix(rust): binary search snippet

--- snippets/src/search.rs
+++ snippets/src/search.rs
@@ -1,28 +1,46 @@
 use cargo_snippet::snippet;
 
-mod usize {
+#[snippet(name = ";search_binary_int")]
+fn binary_search_int<T, F>(mut ng: T, mut ok: T, f: F) -> T
+where
+    T: num::Integer + num::FromPrimitive + Copy,
+    F: Fn(T) -> bool,
+{
+    while ng.min(ok) + T::one() < ok.max(ng) {
+        let mid = (ng + ok).div(T::from_u8(2).unwrap());
+        if f(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    ok
+}
+
+#[snippet(name = ";search_binary_f64")]
+fn binary_search_float<F>(mut ng: f64, mut ok: f64, eps: f64, f: F) -> f64
+where
+    F: Fn(f64) -> bool,
+{
+    while (ok - ng).abs() > eps {
+        let mid = (ok + ng) / 2.;
+        if f(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    ok
+}
+
+#[cfg(test)]
+mod tests {
+
     use super::*;
 
-    #[snippet(name = ";search_binary_int")]
-    fn binary_search_int<T, F>(mut ng: T, mut ok: T, f: F) -> T
-    where
-        T: num::Integer + num::FromPrimitive + Copy,
-        F: Fn(T) -> bool,
-    {
-        let diff = if ok > ng { ok - ng } else { ng - ok };
-        while diff > T::one() {
-            let mid = (ng + ok).div(T::from_u8(2).unwrap());
-            if f(mid) {
-                ok = mid;
-            } else {
-                ng = mid;
-            }
-        }
-        ok
-    }
-
     #[test]
-    fn test_binary_search() {
+    fn test_binary_search_int() {
+        binary_search_int(0, 10, |x| x >= 5);
         assert_eq!(binary_search_int(0, 10, |x| x >= 5), 5);
         assert_eq!(binary_search_int(0, 10, |x| x >= 10), 10);
         assert_eq!(binary_search_int(0, 10, |x| x >= 11), 10);
@@ -34,48 +52,30 @@ mod usize {
         assert_eq!(binary_search_int(0, 10, |x| x >= 8), 8);
         assert_eq!(binary_search_int(0, 10, |x| x >= 2), 2);
         assert_eq!(binary_search_int(0, 10, |x| x >= 3), 3);
+
+        assert_eq!(binary_search_int(10, 0, |x| x <= 5), 5);
     }
-}
 
-mod f64 {
-    use super::*;
-
-    #[snippet(name = ";search_binary_f64")]
-    fn binary_search<F>(mut ng: f64, mut ok: f64, eps: f64, f: F) -> f64
-    where
-        F: Fn(f64) -> bool,
-    {
-        while (ok - ng).abs() > eps {
-            let mid = (ok + ng) / 2.;
-            if f(mid) {
-                ok = mid;
-            } else {
-                ng = mid;
+    macro_rules! assert_delta {
+        ($x:expr, $y:expr, $d:expr) => {
+            if !($x - $y < $d || $y - $x < $d) {
+                panic!();
             }
-        }
-        ok
+        };
     }
 
     #[test]
-    fn test_binary_search() {
-        macro_rules! assert_delta {
-            ($x:expr, $y:expr, $d:expr) => {
-                if !($x - $y < $d || $y - $x < $d) {
-                    panic!();
-                }
-            };
-        }
-
-        assert_delta!(binary_search(0., 10., 1e-6, |x| x >= 5.), 5., 1e-6);
-        assert_delta!(binary_search(0., 10., 1e-6, |x| x >= 10.), 10., 1e-6);
-        assert_delta!(binary_search(0., 10., 1e-6, |x| x >= 11.), 10., 1e-6);
-        assert_delta!(binary_search(0., 10., 1e-6, |x| x >= 9.), 9., 1e-6);
-        assert_delta!(binary_search(0., 10., 1e-6, |x| x >= 1.), 1., 1e-6);
-        assert_delta!(binary_search(0., 10., 1e-6, |x| x >= 4.), 4., 1e-6);
-        assert_delta!(binary_search(0., 10., 1e-6, |x| x >= 6.), 6., 1e-6);
-        assert_delta!(binary_search(0., 10., 1e-6, |x| x >= 7.), 7., 1e-6);
-        assert_delta!(binary_search(0., 10., 1e-6, |x| x >= 8.), 8., 1e-6);
-        assert_delta!(binary_search(0., 10., 1e-6, |x| x >= 2.), 2., 1e-6);
-        assert_delta!(binary_search(0., 10., 1e-6, |x| x >= 3.), 3., 1e-6);
+    fn test_binary_search_float() {
+        assert_delta!(binary_search_float(0., 10., 1e-6, |x| x >= 5.), 5., 1e-6);
+        assert_delta!(binary_search_float(0., 10., 1e-6, |x| x >= 10.), 10., 1e-6);
+        assert_delta!(binary_search_float(0., 10., 1e-6, |x| x >= 11.), 10., 1e-6);
+        assert_delta!(binary_search_float(0., 10., 1e-6, |x| x >= 9.), 9., 1e-6);
+        assert_delta!(binary_search_float(0., 10., 1e-6, |x| x >= 1.), 1., 1e-6);
+        assert_delta!(binary_search_float(0., 10., 1e-6, |x| x >= 4.), 4., 1e-6);
+        assert_delta!(binary_search_float(0., 10., 1e-6, |x| x >= 6.), 6., 1e-6);
+        assert_delta!(binary_search_float(0., 10., 1e-6, |x| x >= 7.), 7., 1e-6);
+        assert_delta!(binary_search_float(0., 10., 1e-6, |x| x >= 8.), 8., 1e-6);
+        assert_delta!(binary_search_float(0., 10., 1e-6, |x| x >= 2.), 2., 1e-6);
+        assert_delta!(binary_search_float(0., 10., 1e-6, |x| x >= 3.), 3., 1e-6);
     }
 }
