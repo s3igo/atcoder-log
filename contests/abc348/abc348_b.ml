2e15ed0d3c0b5e5329a4a9034178cacac901f294
s3igo <85787242+s3igo@users.noreply.github.com>
2024-05-04 18:00:02 +0900

    refactor(ocaml): ocamlformat config

--- contests/abc348/abc348_b.ml
+++ contests/abc348/abc348_b.ml
@@ -8,15 +8,10 @@ let diff (x1, y1) (x2, y2) = Int.pow (x1 - x2) 2 + Int.pow (y1 - y2) 2
 
 let () =
   let _ = read_line () in
-  let xy =
-    read_lines ()
-    |> List.map ~f:(fun s -> string_to_int_list s |> list_to_tuple2)
-  in
+  let xy = read_lines () |> List.map ~f:(fun s -> string_to_int_list s |> list_to_tuple2) in
   List.(
     map xy ~f:(fun p -> (p, xy)) >>| fun ((x0, y0), l) ->
     mapi l ~f:(fun i e -> (i + 1, e))
     |> fold ~init:(0, 0) ~f:(fun acc (i, (x, y)) ->
-           match diff (x0, y0) (x, y) with
-           | d when d > snd acc -> (i, d)
-           | _ -> acc))
+           match diff (x0, y0) (x, y) with d when d > snd acc -> (i, d) | _ -> acc))
   |> List.iter ~f:(fun (i, _) -> printf "%d\n" i)
