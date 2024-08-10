ffa3c60ce63f0a9a21f584a4a509c948fdf718fc
s3igo <85787242+s3igo@users.noreply.github.com>
2024-08-10 21:12:06 +0900

    feat(log): create skeleton for cli args

---
+++ crates/procon-log/src/args.rs
@@ -0,0 +1,96 @@
+use std::path::PathBuf;
+
+use bpaf::{literal, Bpaf, Parser};
+
+macro_rules! generate_parsers {
+    ($($lang:ident),*) => {
+        $(
+            fn $lang() -> impl Parser<bool> {
+                literal(stringify!($lang)).map(|_| true)
+            }
+        )*
+    };
+}
+
+generate_parsers!(ocaml, rust, haskell, nim);
+
+#[derive(Debug, Clone, Bpaf)]
+pub enum Lang {
+    Rust {
+        #[bpaf(external)]
+        rust: bool,
+    },
+    Ocaml {
+        #[bpaf(external)]
+        ocaml: bool,
+    },
+    Haskell {
+        #[bpaf(external)]
+        haskell: bool,
+    },
+    Nim {
+        #[bpaf(external)]
+        nim: bool,
+    },
+}
+
+#[derive(Debug, Clone, Bpaf)]
+#[bpaf(options, version)]
+pub enum Args {
+    #[bpaf(command)]
+    Open {
+        #[bpaf(external)]
+        lang: Lang,
+        #[bpaf(positional("FILE"))]
+        file: PathBuf,
+    },
+    #[bpaf(command)]
+    Test {
+        #[bpaf(external)]
+        lang: Lang,
+        #[bpaf(positional("FILE"))]
+        file: PathBuf,
+    },
+    #[bpaf(command)]
+    Submit {
+        #[bpaf(external)]
+        lang: Lang,
+        #[bpaf(positional("FILE"))]
+        file: PathBuf,
+    },
+    #[bpaf(command)]
+    Save {
+        #[bpaf(external)]
+        lang: Lang,
+        #[bpaf(positional("FILE"))]
+        file: PathBuf,
+    },
+    #[bpaf(command)]
+    Clear {
+        #[bpaf(external)]
+        lang: Lang,
+        #[bpaf(positional("FILE"))]
+        file: PathBuf,
+    },
+    Version {
+        /// Print version information
+        #[bpaf(long, short('V'), switch)]
+        version: bool,
+    },
+}
+
+impl Args {
+    pub fn parse() -> Self {
+        args().run()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_args_invariants() {
+        args().check_invariants(false);
+    }
+}
--- crates/procon-log/src/main.rs
+++ crates/procon-log/src/main.rs
@@ -1,3 +1,24 @@
+use procon_log::args::Args;
+
 fn main() {
-    println!("Hello, world!");
+    match Args::parse() {
+        Args::Open { lang, file } => {
+            println!("Opening file {:?} for language {:?}", file, lang);
+        },
+        Args::Test { lang, file } => {
+            println!("Testing file {:?} for language {:?}", file, lang);
+        },
+        Args::Submit { lang, file } => {
+            println!("Submitting file {:?} for language {:?}", file, lang);
+        },
+        Args::Save { lang, file } => {
+            println!("Saving file {:?} for language {:?}", file, lang);
+        },
+        Args::Clear { lang, file } => {
+            println!("Clearing file {:?} for language {:?}", file, lang);
+        },
+        Args::Version { .. } => {
+            println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+        },
+    }
 }
