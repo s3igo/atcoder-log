b5793c9fa85aadd9a9a9e6e38a52ce2b3f4e8295
s3igo <85787242+s3igo@users.noreply.github.com>
2024-08-19 11:51:50 +0900

    feat: add migrate script for solutions of 1.70.0

--- scripts/migrate-directory-structure.ts
+++ scripts/migrate-directory-structure.ts
@@ -1,5 +1,7 @@
 import { ensureDir, move, walk } from "jsr:@std/fs@1.0.1";
 
+const contestRoot = "./contests";
+
 const template = `use proconio::input;
 
 fn main() {
@@ -7,7 +9,7 @@ fn main() {
 }
 `;
 
-async function processFiles(baseDir: string) {
+async function _processFiles(baseDir: string) {
     for await (const entry of walk(baseDir, { exts: [".rs"] })) {
         if (entry.isFile) {
             const filePath = entry.path;
@@ -26,10 +28,10 @@ async function processFiles(baseDir: string) {
                 const fileContent = await Deno.readTextFile(filePath);
                 if (fileContent.trim() !== template.trim()) {
                     const newFilePath =
-                        `${contestDir}/${contestDir}_${name}.rs`;
+                        `${contestRoot}/${contestDir}/${contestDir}_${name}.rs`;
                     console.log(`New file path: ${newFilePath}`);
 
-                    await ensureDir(contestDir);
+                    await ensureDir(`${contestRoot}/${contestDir}`);
                     await move(filePath, newFilePath);
                     console.log(`Moved ${filePath} to ${newFilePath}`);
                 }
@@ -38,4 +40,50 @@ async function processFiles(baseDir: string) {
     }
 }
 
-//await processFiles("./contests/1.42.0");
+//await processFiles(`${contestRoot}/1.42.0`);
+
+const template2 = `use proconio::input;
+
+fn main() {
+    input!();
+}
+`;
+
+async function processFilesFor_1_70_0(baseDir: string) {
+    for await (const entry of walk(baseDir, { exts: [".rs"] })) {
+        if (entry.isFile) {
+            const filePath = entry.path;
+            console.log(`\nProcessing ${filePath}`);
+
+            const fileName = filePath.split("/").pop();
+            console.log(`File name: ${fileName}`);
+
+            const contestDir = filePath.split("/")[2];
+            console.log(`Contest dir: ${contestDir}`);
+
+            const name = fileName?.split(".")[0];
+            console.log(`Name: ${name}`);
+
+            if (
+                fileName && fileName.endsWith(".rs") &&
+                contestDir !== "tessoku-book" // Skip the tessoku-book directory
+            ) {
+                const fileContent = await Deno.readTextFile(filePath);
+                if (
+                    fileContent.trim() !== template.trim() &&
+                    fileContent.trim() !== template2.trim()
+                ) {
+                    const newFilePath =
+                        `${contestRoot}/${contestDir}/${contestDir}_${name}.rs`;
+                    console.log(`New file path: ${newFilePath}`);
+
+                    await ensureDir(`${contestRoot}/${contestDir}`);
+                    await move(filePath, newFilePath);
+                    console.log(`Moved ${filePath} to ${newFilePath}`);
+                }
+            }
+        }
+    }
+}
+
+await processFilesFor_1_70_0(`${contestRoot}/1.70.0`);
