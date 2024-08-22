88a6f3d67a23bfad7809e4dcb06ae5fab53ecaf8
s3igo <85787242+s3igo@users.noreply.github.com>
2024-08-22 21:11:34 +0900

    fix: file hash

--- crates/procon-log/toolchain.nix
+++ crates/procon-log/toolchain.nix
@@ -2,5 +2,5 @@
 
 fenix'.fromToolchainFile {
   file = ./rust-toolchain.toml;
-  sha256 = "sha256-Ngiz76YP4HTY75GGdH2P+APE/DEIx2R/Dn+BwwOyzZU=";
+  sha256 = "sha256-3jVIIf5XPnUU1CRaTyAiO0XHVbJl12MSx3eucTXCjtE=";
 }
