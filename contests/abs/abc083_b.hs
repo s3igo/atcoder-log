8c70b687fba12021290134f24dda262c35521849
s3igo <85787242+s3igo@users.noreply.github.com>
2024-09-11 21:26:01 +0900

    refactor: abs in haskell

--- contests/abs/abc083_b.hs
+++ contests/abs/abc083_b.hs
@@ -1,18 +1,10 @@
-import Data.ByteString.Char8 qualified as BS
-import Data.Char (isSpace)
-import Data.List
+import Data.Array (Ix (inRange))
 
-readInts :: IO [Int]
-readInts = unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
-
-contains :: (Int, Int) -> Int -> Bool
-(l, r) `contains` n = l <= n && n <= r
-
-digitsSum :: Int -> Int
-digitsSum 0 = 0
-digitsSum n = n `mod` 10 + digitsSum (n `div` 10)
+digits :: Int -> [Int]
+digits 0 = []
+digits n = n `mod` 10 : digits (n `div` 10)
 
 main :: IO ()
 main = do
-  [n, a, b] <- readInts
-  print $ sum [i | i <- [1 .. n], (a, b) `contains` digitsSum i]
+  [n, a, b] <- map (read @Int) . words <$> getLine
+  print $ sum [i | i <- [1 .. n], inRange (a, b) (sum $ digits i)]
